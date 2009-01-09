e0f5c28fb38024b9e4a6ed799f7b426fa037ee46
Darcy Mason <darcymason@gmail.com>
2009-01-09 02:01:07 +0000

    More fine-tuning for 0.9.2 package. Moved license.txt and README.txt, adjusted MANIFEST.in.

--- source/dicom/__init__.py
+++ source/dicom/__init__.py
@@ -17,21 +17,20 @@ GNU General Public License (license.txt) for more details
 -----------
 Quick Start
 -----------
-1. Get and install Python (www.python.org). If you are on Windows, try the
-(free) ActivePython distribution (www.activestate.com/Products/ActivePython).
-
-2. Here is a very simple program to read a dicom file, modify a value, and
-write the result to a new dicom file::
+1. A simple program to read a dicom file, modify a value, and write to a new file::
     import dicom
     dataset = dicom.ReadFile("file1.dcm")
     dataset.PatientsName = 'anonymous'
-    dicom.WriteFile("file2.dcm", dataset)
+    dataset.SaveAs("file2.dcm")
     
-3. See the files in the examples directory that came with this package,
-in particular the capture of an interactive session.
+2. See the files in the examples directory that came with this package for more
+examples, including some interactive sessions.
 
-4. Learn the methods of the Dataset class; that is the one you will
+3. Learn the methods of the Dataset class; that is the one you will
 work with most directly.
+
+4. Questions/comments etc can be directed to the pydicom google group at
+http://groups.google.com/group/pydicom
 """
 # Set up logging system for the whole package. 
 # In each module, set logger=logging.getLogger('pydicom')  and the same instance will be used by all
