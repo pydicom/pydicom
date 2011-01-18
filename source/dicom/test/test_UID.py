b1528f6fc6f9dbefb5f830b6914554815598166e
Darcy Mason <darcymason@gmail.com>
2011-01-17 21:42:27 -0500

    Fix UID comparisons against non-string values. Fixes issue 96.

--- source/dicom/test/test_UID.py
+++ source/dicom/test/test_UID.py
@@ -32,6 +32,16 @@ class UIDtests(unittest.TestCase):
         uid = UID('1.2.840.10008.1.2')
         self.assertEqual(uid, 'Implicit VR Little Endian', "UID equality failed on name")
         self.assertEqual(uid, '1.2.840.10008.1.2', "UID equality failed on number string")
+    def testCompareNumber(self):
+        """UID: comparing against a number give False............."""
+        # From issue 96
+        uid = UID('1.2.3')
+        self.assertNotEqual(uid, 3, "Comparison against a number returned True")
+    def testCompareNone(self):
+        """UID: comparing against None give False................."""
+        # From issue 96
+        uid = UID('1.2.3')
+        self.assertNotEqual(uid, None, "Comparison against a number returned True")
     def testTransferSyntaxes(self):
         pass
         
