36041f16d824b1a427909191c837402bdb613ff9
Darcy Mason <darcymason@gmail.com>
2013-01-14 22:32:23 -0500

    Prep 0.9.8 release. Update version strings. Some pep8 changes. Note: fails IS limit unit tests on Win 64

--- source/dicom/test/all.bat
+++ source/dicom/test/all.bat
@@ -1,20 +1,16 @@
 echo OFF
 echo all.bat
 echo run pydicom test suite on all supported python versions
-echo ------- python 2.4 ------------
-c:\python24\python run_tests.py
-echo ------- python 2.5 ------------
-c:\python25\python run_tests.py
 echo ------- python 2.6 ------------
 c:\python26\python run_tests.py
+echo ------- python 2.7 ------------
+c:\python27\python run_tests.py
 
 REM Check location for each version -- to make sure are not running old pydicom versions
 echo -
 echo -----------------
 echo Check locations, make sure not pointing to old pydicom code:
-echo Python 2.4
-c:\python24\python -c "import dicom; print dicom.__file__"
-echo Python 2.5
-c:\python25\python -c "import dicom; print dicom.__file__"
 echo Python 2.6
 c:\python26\python -c "import dicom; print dicom.__file__"
+echo Python 2.7
+c:\python27\python -c "import dicom; print dicom.__file__"
--- source/setup.py
+++ source/setup.py
@@ -14,19 +14,20 @@ extra = {}
 #if sys.version_info >= (3,):
 #    extra['use_2to3'] = True
 
-setup(name="pydicom",
-      packages = find_packages(),
-      include_package_data = True,
-      version="0.9.8rc1",
-      package_data = {'dicom': ['testfiles/*.dcm']},
-      zip_safe = False, # want users to be able to see included examples,tests
-      description="Pure python package for DICOM medical file reading and writing",
-      author="Darcy Mason",
-      author_email="darcymason@gmail.com",
-      url="http://pydicom.googlecode.com",
-      license = "MIT license",
-      keywords = "dicom python medical imaging",
-      classifiers = [
+setup(
+    name="pydicom",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.9.8",
+    package_data={'dicom': ['testfiles/*.dcm']},
+    zip_safe=False,  # want users to be able to see included examples,tests
+    description="Pure python package for DICOM medical file reading and writing",
+    author="Darcy Mason",
+    author_email="darcymason@gmail.com",
+    url="http://pydicom.googlecode.com",
+    license="MIT license",
+    keywords="dicom python medical imaging",
+    classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Intended Audience :: Healthcare Industry",
@@ -43,22 +44,22 @@ setup(name="pydicom",
         "Topic :: Scientific/Engineering :: Medical Science Apps.",
         "Topic :: Scientific/Engineering :: Physics",
         "Topic :: Software Development :: Libraries",
-        ],
-      long_description = """
-      pydicom is a pure python package for parsing DICOM files.
-      DICOM is a standard (http://medical.nema.org) for communicating
-      medical images and related information such as reports
-      and radiotherapy objects.
+    ],
+    long_description="""
+    pydicom is a pure python package for parsing DICOM files.
+    DICOM is a standard (http://medical.nema.org) for communicating
+    medical images and related information such as reports
+    and radiotherapy objects.
 
-      pydicom makes it easy to read these complex files into natural
-      pythonic structures for easy manipulation.
-      Modified datasets can be written again to DICOM format files.
-      See the `Getting Started <http://code.google.com/p/pydicom/wiki/GettingStarted>`_
-      wiki page for installation and basic information, and the
-      `Pydicom User Guide <http://code.google.com/p/pydicom/wiki/PydicomUserGuide>`_ page
-      for an overview of how to use the pydicom library.
-      """,
-      test_loader = "dicom.test.run_tests:MyTestLoader",
-      test_suite = "dummy_string",
-      **extra
-     )
+    pydicom makes it easy to read these complex files into natural
+    pythonic structures for easy manipulation.
+    Modified datasets can be written again to DICOM format files.
+    See the `Getting Started <http://code.google.com/p/pydicom/wiki/GettingStarted>`_
+    wiki page for installation and basic information, and the
+    `Pydicom User Guide <http://code.google.com/p/pydicom/wiki/PydicomUserGuide>`_ page
+    for an overview of how to use the pydicom library.
+    """,
+    test_loader="dicom.test.run_tests:MyTestLoader",
+    test_suite="dummy_string",
+    **extra
+)
