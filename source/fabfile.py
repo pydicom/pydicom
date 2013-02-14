ac86c789d966c942f1a2d329c5342abac888e668
Darcy Mason <darcymason@gmail.com>
2013-02-13 21:54:10 -0500

    Generalize fabfile and expand tox to py31

--- source/fabfile.py
+++ source/fabfile.py
@@ -4,17 +4,26 @@
 from fabric.api import local, settings, abort
 from fabric.contrib.console import confirm
 
+import os.path
+import dicom
+pydicompath = dicom.__path__[0]
+tox_ini = os.path.join(pydicompath, "../tox.ini")
+
 
 def syntax():
-    local("reindent -r dicom")
+    local("reindent -r " + pydicompath)
     with settings(warn_only=True):
-        result = local("flake8 --ignore=E501 dicom")
+        result = local("flake8 --ignore=E501 " + pydicompath)
     if result.failed and not confirm("Continue?"):
         abort("Aborted due to user request")
 
 
 def test():
     local("python setup.py --quiet test")
+
+
+def fulltest():
+    local("tox -c" + tox_ini)
 
 
 def commit():
