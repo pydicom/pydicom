d8bd28c7810f97af45f234b1016be31ca2d42382
Yaroslav Halchenko <debian@onerussian.com>
2015-01-17 00:15:48 -0500

    BF: do not force unicode names when not supported

--- source/dicom/test/test_unicode.py
+++ source/dicom/test/test_unicode.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import dicom
+import sys
 import unittest
 
 
@@ -9,6 +10,14 @@ class UnicodeFilenames(unittest.TestCase):
     def testRead(self):
         """Unicode: Can read a file with unicode characters in name................"""
         uni_name = u'test°'
+
+        # verify first that we could encode file name in this environment
+        try:
+            _ = uni_name.encode(sys.getfilesystemencoding())
+        except UnicodeEncodeError:
+            print("SKIP: Environment doesn't support unicode filenames")
+            return
+
         try:
             dicom.read_file(uni_name)
         except UnicodeEncodeError:
