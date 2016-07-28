ef5b7900e436d01da34588be28b8219bf9fae2b8
rhaxton <robert.haxton@gmail.com>
2016-07-28 15:06:39 -0700

    See #279 - adding test case of file with ISO 2022 IR 6\IOS2022 IR 87 character set encoding

--- tests/charset_files/charlist.py
+++ tests/charset_files/charlist.py
@@ -21,8 +21,13 @@ if __name__ == "__main__":
     for name in names:
         ds = pydicom.read_file(name)
         ds.decode()
-        files_info.append((name, ds.SpecificCharacterSet, ds.PatientsName))
-
+        try:
+            files_info.append((name, ds.SpecificCharacterSet, ds.PatientName))
+        except Exception:
+            try:
+                files_info.append((name, ds.RequestedProcedureCodeSequence[0].SpecificCharacterSet, ds.RequestedProcedureCodeSequence[0].PatientName))
+            except Exception:
+                logger.warning("Trouble reading file %s", name)
     # Show the information
     format = "%-16s %-40s %s"
     logging.info(format % ("Filename", "Character Sets", "Patient's Name"))
