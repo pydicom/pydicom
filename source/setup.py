a78172227c5561fcb746b91587daa5e0ac1b2f8a
Darcy Mason <darcymason@gmail.com>
2017-01-23 23:16:47 -0500

    Updates setup.py for creation of new 'dicom' package release

--- source/setup.py
+++ source/setup.py
@@ -18,16 +18,16 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
 setup(
-    name="pydicom",
+    name="dicom",
     packages=find_packages(),
     include_package_data=True,
-    version="0.9.9",
+    version="0.9.9-1",
     package_data={'dicom': ['testfiles/*.dcm']},
     zip_safe=False,  # want users to be able to see included examples,tests
     description="Pure python package for DICOM medical file reading and writing",
     author="Darcy Mason and contributors",
     author_email="darcymason@gmail.com",
-    url="http://pydicom.googlecode.com",
+    url="https://github.com/darcymason/pydicom",
     license="MIT license",
     keywords="dicom python medical imaging",
     classifiers=[
@@ -49,18 +49,8 @@ setup(
         "Topic :: Software Development :: Libraries",
     ],
     long_description="""
-    pydicom is a pure python package for parsing DICOM files.
-    DICOM is a standard (http://medical.nema.org) for communicating
-    medical images and related information such as reports
-    and radiotherapy objects.
-
-    pydicom makes it easy to read these complex files into natural
-    pythonic structures for easy manipulation.
-    Modified datasets can be written again to DICOM format files.
-    See the `Getting Started <http://code.google.com/p/pydicom/wiki/GettingStarted>`_
-    wiki page for installation and basic information, and the
-    `Pydicom User Guide <http://code.google.com/p/pydicom/wiki/PydicomUserGuide>`_ page
-    for an overview of how to use the pydicom library.
+    dicom is a historical version of the pydicom package, for versions < 1.0.
+    New code should target the pydicom package rather than this one.
     """,
     test_loader="dicom.test.run_tests:MyTestLoader",
     test_suite="dummy_string",
