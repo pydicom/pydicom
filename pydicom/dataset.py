ab79f60b8abc18b4106247f4ddbd287376b51387
scaramallion <scaramallion@users.noreply.github.com>
2017-03-02 12:48:17 +1100

    Reordered Dataset docstring examples

--- pydicom/dataset.py
+++ pydicom/dataset.py
@@ -91,13 +91,14 @@ class Dataset(dict):
     Add DataElements to the Dataset (for elements in the DICOM dictionary).
     >>> ds = Dataset()
     >>> ds.PatientName = "CITIZEN^Joan"
-    >>> ds.PatientID = '12345'
+    >>> ds.add_new(0x00100020, 'LO', '12345')
     >>> ds[0x0010, 0x0030] = DataElement(0x00100030, 'DA', '20010101')
 
     Add Sequence DataElement to the Dataset
-    >>> ds.BeamSequence = [Dataset(), Dataset()]
+    >>> ds.BeamSequence = [Dataset(), Dataset(), Dataset()]
     >>> ds.BeamSequence[0].Manufacturer = "Linac, co."
     >>> ds.BeamSequence[1].Manufacturer = "Linac and Sons, co."
+    >>> ds.BeamSequence[2].Manufacturer = "Linac and Daughters, co."
 
     Add private DataElements to the Dataset
     >>> ds.add(DataElement(0x0043102b, 'SS', [4, 4, 0, 0]))
@@ -112,11 +113,35 @@ class Dataset(dict):
     >>> ds.PatientName
     'CITIZEN^John'
 
+    Retrieving a DataElement's value from a Sequence
+    >>> ds.BeamSequence[0].Manufacturer
+    'Linac, co.'
+    >>> ds.BeamSequence[1].Manufacturer
+    'Linac and Sons, co.'
+
     Retrieving DataElements
     >>> elem = ds[0x00100010]
     >>> elem = ds.data_element('PatientName')
     >>> elem
     (0010, 0010) Patient's Name                      PN: 'CITIZEN^Joan'
+
+    Deleting a DataElement from the Dataset
+    >>> del ds.PatientID
+    >>> del ds.BeamSequence[1].Manufacturer
+    >>> del ds.BeamSequence[2]
+
+    Deleting a private DataElement from the Dataset
+    >>> del ds[0x0043, 0x102b]
+
+    Determining if a DataElement is present in the Dataset
+    >>> 'PatientName' in ds
+    True
+    >>> 'PatientID' in ds
+    False
+    >>> 0x00100030 in ds
+    True
+    >>> 'Manufacturer' in ds.BeamSequence[0]
+    True
 
     Iterating through the top level of a Dataset only (excluding Sequences)
     >>> for elem in ds:
@@ -133,29 +158,6 @@ class Dataset(dict):
     >>>             [recurse(item) for item in elem]
     >>>         else:
     >>>             # Do something useful with each DataElement
-
-    Retrieving a DataElement's value from a Sequence
-    >>> ds.BeamSequence[0].Manufacturer
-    'Linac, co.'
-    >>> ds.BeamSequence[1].Manufacturer
-    'Linac and Sons, co.'
-
-    Deleting a DataElement from the Dataset
-    >>> del ds.PatientID
-    >>> del ds.BeamSequence[1].Manufacturer
-
-    Deleting a private DataElement from the Dataset
-    >>> del ds[0x0043, 0x102b]
-
-    Determining if a DataElement is present in the Dataset
-    >>> 'PatientName' in ds
-    True
-    >>> 'PatientID' in ds
-    False
-    >>> 0x00100030 in ds
-    True
-    >>> 'Manufacturer' in ds.BeamSequence[0]
-    True
 
     Attributes
     ----------
