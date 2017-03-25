e62d1a68c10879fe1527ac3d8789b381220e1023
scaramallion <scaramallion@users.noreply.github.com>
2017-03-25 18:47:02 +1100

    Added failing unit test, updated exception msg

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -59,10 +59,25 @@ class DatasetTests(unittest.TestCase):
         def callable_pixel_array():
             ds.pixel_array
 
-        attribute_error_msg = "AttributeError in pixel_array property: " + \
-            "Dataset does not have attribute 'TransferSyntaxUID'"
-        self.failUnlessExceptionArgs(attribute_error_msg,
-                                     PropertyError, callable_pixel_array)
+        msg = "AttributeError in pixel_array property: " + \
+            "'Dataset' object has no attribute 'TransferSyntaxUID'"
+        self.failUnlessExceptionArgs(msg, PropertyError, callable_pixel_array)
+
+    def test_attribute_error_in_property_correct_debug(self):
+        """Test AttributeError in property raises correctly."""
+        class Foo(Dataset):
+            @property
+            def bar(self): return self._barr()
+
+            def _bar(self): return 'OK'
+
+        def test():
+            ds = Foo()
+            ds.bar
+
+        self.assertRaises(AttributeError, test)
+        msg = "'Foo' object has no attribute '_barr'"
+        self.failUnlessExceptionArgs(msg, AttributeError, test)
 
     def testTagExceptionPrint(self):
         # When printing datasets, a tag number should appear in error
