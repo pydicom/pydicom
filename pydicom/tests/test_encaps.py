73e6e852e40ec235df90241726b9ad7ca5c731ab
scaramallion <scaramallion@users.noreply.github.com>
2017-08-11 13:54:20 +1000

    Add test file

---
+++ pydicom/tests/test_encaps.py
@@ -0,0 +1,21 @@
+# Copyright 2008-2017 pydicom authors. See LICENSE file for details.
+"""Test for encaps.py"""
+
+import pytest
+
+from pydicom.encaps import decode_data_sequence, defragment_data, read_item
+
+
+class TestDecodeDataSequence(object):
+    """Test encaps.decode_data_sequence"""
+    pass
+
+
+class TestDefragmentData(object):
+    """Test encaps.defragment_data"""
+    pass
+
+
+class TestReadItem(object):
+    """Test encaps.read_item"""
+    pass
