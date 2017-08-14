15302f4792523045d2cb66cfebcacc6d46f842b3
scaramallion <scaram@gmail.com>
2017-08-14 10:49:07 +1000

    Add tests for get_frame_offsets

--- pydicom/encaps.py
+++ pydicom/encaps.py
@@ -16,10 +16,256 @@
 # If a single frame, it may be 0 length/no value,
 # or it may have a single pointer (0).
 
-import pydicom.config
+from io import BytesIO
+from struct import unpack
 
+import pydicom.config
 from pydicom.filebase import DicomBytesIO
-from pydicom.tag import (ItemTag, SequenceDelimiterTag)
+from pydicom.tag import (Tag, ItemTag, SequenceDelimiterTag)
+
+
+def get_pixel_data_fragments(fp):
+    """Return the encapsulated pixel data fragments as a list of bytes.
+
+    For compressed (encapsulated) Transfer Syntaxes, the (7fe0,0010) 'Pixel
+    Data' element is encoded in an encapsulated format. 
+
+    (7fe0,0010) Pixel Data
+    ----------------------
+    Encoding
+    ~~~~~~~~
+    The encoding of the element shall be explicit VR little endian.
+
+    VR
+    ~~
+    The Pixel Data shall have a VR of 'OB'.
+
+    Length
+    ~~~~~~
+    The element's length shall be 0xFFFFFFFF (i.e. undefined).
+
+    Encapsulation
+    -------------
+    The encoded pixel data stream is fragmented into one or more Items. The
+    stream may represent a single or multi-frame image (whether or not one
+    fragment per frame is permitted or not is defined per Transfer Syntax).
+
+    Each 'Data Stream Fragment' shall have tag of (fffe,e000), followed by a 4
+    byte 'Item Length' field encoding the explicit number of bytes in the Item.
+    All Items containing an encoded fragment shall have an even number of bytes
+    greater than or equal to 2, with the last fragment being padded if
+    necessary.
+
+    The first Item in the 'Sequence of Items' shall be a 'Basic Offset Table',
+    however the Basic Offset Table item value is not required to be present.
+
+    The Sequence of Items is terminated by a Sequence Delimiter Item with tag
+    (fffe,e0dd) and an Item Length field of value 0x00000000.
+
+    Sample Encapsulated Pixel Data
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Pixel Data
+    | Tag     | VR  | Rsvd| Length    |
+    e0 7f 10 00 4f 42 00 00 ff ff ff ff
+
+    Item: Basic Offset Table (40 bytes item length)
+    | Tag     | Length    | Value (0 and 4966)    |
+    fe ff 00 e0 28 00 00 00 00 00 00 00 66 13 00 00
+
+    Item : First Fragment of Frame 1 (4958 bytes item length)
+    | Tag     | Length    | Value --->
+    fe ff 00 e0 5e 13 00 00 ...
+
+    Item : First Fragment of Frame 2 (4742 bytes item length)
+    | Tag     | Length    | Value --->
+    fe ff 00 e0 86 12 00 00 ...
+
+    Item: Sequence Delimiter
+    | Tag     | Length    |
+    fe ff dd e0 00 00 00 00
+
+    Parameters
+    ----------
+    fp : pydicom.filebase.DicomBytesIO
+        The encoded (7fe0,0010) 'Pixel Data' element value, positioned after
+        the Basic Offset Table item.
+
+    Returns
+    -------
+    fragments : list of bytes
+        A list containing the pixel data fragments.
+
+    Raises
+    ------
+    ValueError
+        If the data contains an item with an undefined length or an unknown
+        tag.
+
+    References
+    ----------
+    DICOM Standard Part 5, Annex A.4
+    """
+    # We should be positioned at the start of the Item Tag for the first
+    # fragment after the Basic Offset Table
+    fragments = []
+    at_end_of_stream = False
+    while True:
+        try:
+            tag = Tag(fp.read_tag())
+        except EOFError:
+            break
+
+        if tag == 0xFFFEE000:
+            # Item
+            length = fp.read_UL()
+            if length == 0xFFFFFFFF:
+                raise ValueError("Undefined length at offset %d when parsing "
+                                 "the encapsulated pixel data"
+                                 .format(fp.tell() - 4))
+            fragments.append(fp.read(length))
+        elif tag == 0xFFFEE0DD:
+            # Sequence Delimiter Item
+            break
+        else:
+            raise ValueError("Unknown tag '%s' at offset %d when parsing the "
+                             "encapsulated pixel data"
+                             .format(tag, fp.tell() - 4))
+
+    return fragments
+
+def get_frame_offsets(fp):
+    """Return a list of the fragment offsets from the Basic Offset Table.
+
+    For single or multi-frame images with only one frame this will return
+    an empty list. For multi-frame images with multiple frames this will return
+    a list containing the byte offsets to the first fragment of each frame, as
+    measured from the end of the Basic Offset Table Item.
+
+    Basic Offset Table
+    ~~~~~~~~~~~~~~~~~~
+    The Basic Offset Table Item must be present and have a tag (FFEE,E000) and
+    a length, however it may or may not have a value.
+    
+    For single or multi-frame images with only one frame, the Basic Offset
+    Table may or may not have a value.
+
+    For multi-frame images with more than one frame, the Basic Offset Table
+    should have a value.
+    
+    When the Basic Offset Table has no value then its length shall be
+    0x00000000. When it has a value, then it shall contain concatenated 32-bit
+    unsigned integer values that are the byte offsets to the first byte of the
+    Item tag of the first fragment in each frame in the Sequence of Items.
+    These offsets are measured from the first byte of the first item tag
+    following the Basic Offset Table Item.
+
+    All decoders, both for single and multi-frame images should accept both
+    an empty Basic Offset Table and one containing offset values.
+
+    Parameters
+    ----------
+    fp : pydicom.filebase.DicomBytesIO
+        The encapsulated pixel data positioned at the start of the Basic Offset
+        Table.
+
+    Returns
+    -------
+    list of int
+        The byte offsets to the first fragment of each frame, as measured from
+        the end of the Basic Offset Table item.
+
+    Raises
+    ------
+    ValueError
+        If the Basic Offset Table item's tag is not (FFEE,E000) or if the length
+        of the item in bytes is not a multiple of 4.
+    """
+    tag = Tag(fp.read_tag())
+    if tag != 0xfffee000:
+        raise ValueError("Unknown tag '%s' at offset %d when parsing the "
+                         "encapsulated pixel data" %(tag, fp.tell() - 4))
+
+    length = fp.read_UL()
+    if length % 4:
+        raise ValueError("The length of the Basic Offset Table item is not "
+                           "a multiple of 4.")
+
+    value = []
+    for ii in range(length / 4):
+        value.append(fp.read_UL())
+
+    return value
+
+def get_pixel_data_frames(bytestream):
+    """Return the encapsulated pixel data frame(s) as a list of bytearray.
+
+    For the following transfer syntaxes, a fragment may not contain encoded
+    data from more than one frame. However data from one frame may span
+    multiple fragments.
+
+    1.2.840.10008.1.2.4.50 - JPEG Baseline (Process 1)
+    1.2.840.10008.1.2.4.51 - JPEG Baseline (Process 2 and 4)
+    1.2.840.10008.1.2.4.57 - JPEG Lossless, Non-Hierarchical (Process 14)
+    1.2.840.10008.1.2.4.70 - JPEG Lossless, Non-Hierarchical, First-Order
+        Prediction (Process 14 [Selection Value 1])
+    1.2.840.10008.1.2.4.80 - JPEG-LS Lossless Image Compression
+    1.2.840.10008.1.2.4.81 - JPEG-LS Lossy (Near-Lossless) Image Compression
+    1.2.840.10008.1.2.4.90 - JPEG 2000 Image Compression (Lossless Only)
+    1.2.840.10008.1.2.4.91 - JPEG 2000 Image Compression
+    1.2.840.10008.1.2.4.92 - JPEG 2000 Part 2 Multi-component Image Compression
+        (Lossless Only)
+    1.2.840.10008.1.2.4.93 - JPEG 2000 Part 2 Multi-component Image Compression
+
+    For the following transfer syntaxes, each frame shall be encoded in one and
+    only one fragment.
+
+    1.2.840.10008.1.2.5 - RLE Lossless
+
+    Parameters
+    ----------
+    bytestream : str or bytes
+        The encapsulated pixel data byte stream.
+
+    Returns
+    -------
+    list of bytearray
+        The frame(s) contained in the encapsulated pixel data.
+
+    References
+    ----------
+    DICOM Standard Part 5, Annex A
+    """
+    fp = DicomBytesIO(bytestream)
+    fp.is_little_endian = True
+    
+    # offsets contains the byte offset for the first fragment in each frame
+    offsets = parse_basic_offset_table(fp)
+    # - N * (8 bytes for item tag and length), where N is fragment number
+    # FIXME: This is wrong, only works when 1:1 ratio of fragment to frame
+    offsets = [val - ii * 8 for ii, val in enumerate(offsets)]
+    fragments = parse_pixel_data_fragments(fp)
+
+    frames = []
+    if offsets:
+        # We start at the second offset so we'll need to do the final frame
+        # once we've iterated through the offsets
+        fragment_no = 0
+        frame_length = 0
+        for frame_offset in offsets[1:]:
+            frame = bytearray()
+            while frame_length < frame_offset:
+                frame.extend(fragments[fragment_no])
+                frame_length += len(fragments[fragment_no])
+                fragment_no += 1
+
+            frames.append(frame)
+
+        # Final frame
+        frames.append(bytearray(b''.join(fragments[fragment_no:])))
+    else:
+        frames.append(bytearray(b''.join(fragments)))
+
+    return frames
 
 
 def decode_data_sequence(data):
@@ -56,19 +302,17 @@ def decode_data_sequence(data):
 
 
 def defragment_data(data):
-    """Read encapsulated data and return one continuous string.
+    """Read encapsulated data and return the fragments as one continuous string.
 
     Parameters
     ----------
-    data : list of str
-        String of encapsulated data, typically dataset.PixelData. If PixelData
-        has multiple frames, then should separate out before calling this
-        routine.
+    data : list of bytes
+        The encapsulated pixel data fragments.
 
     Returns
     -------
     bytes
-        All fragments concatenated together as a byte string.
+        All fragments concatenated together.
     """
     return b"".join(decode_data_sequence(data))
 
--- pydicom/tests/test_encaps.py
+++ pydicom/tests/test_encaps.py
@@ -3,19 +3,68 @@
 
 import pytest
 
-from pydicom.encaps import decode_data_sequence, defragment_data, read_item
+from pydicom.encaps import (get_pixel_data_fragments, get_frame_offsets,
+                            get_pixel_data_frames)
+from pydicom.filebase import DicomBytesIO
 
 
-class TestDecodeDataSequence(object):
-    """Test encaps.decode_data_sequence"""
+class TestGetPixelDataFragments(object):
+    """Test encaps.get_pixel_data_fragments"""
     pass
 
 
-class TestDefragmentData(object):
-    """Test encaps.defragment_data"""
-    pass
+class TestGetFrameOffsets(object):
+    """Test encaps.get_frame_offsets"""
+    def test_bad_tag(self):
+        """Test raises exception if no item tag."""
+        # (fffe,e100)
+        bytestream = b'\xFE\xFF\x00\xE1' \
+                     b'\x08\x00\x00\x00' \
+                     b'\x01\x02\x03\x04\x05\x06\x07\x08'
+        fp = DicomBytesIO(bytestream)
+        fp.is_little_endian = True
+        pytest.raises(ValueError, get_frame_offsets, fp)
+
+    def test_bad_length(self):
+        """Test raises exception if the item length is not a multiple of 4."""
+        # Length 10
+        bytestream = b'\xFE\xFF\x00\xE0' \
+                     b'\x0A\x00\x00\x00' \
+                     b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A'
+        fp = DicomBytesIO(bytestream)
+        fp.is_little_endian = True
+        pytest.raises(ValueError, get_frame_offsets, fp)
+
+    def test_zero_length(self):
+        """Test reading BOT with zero length"""
+        bytestream = b'\xFE\xFF\x00\xE0' \
+                     b'\x00\x00\x00\x00'
+        fp = DicomBytesIO(bytestream)
+        fp.is_little_endian = True
+        assert [] == get_frame_offsets(fp)
+
+    def test_multi_frame(self):
+        """Test reading multi-frame BOT item"""
+        bytestream = b'\xFE\xFF\x00\xE0' \
+                     b'\x10\x00\x00\x00' \
+                     b'\x00\x00\x00\x00' \
+                     b'\x66\x13\x00\x00' \
+                     b'\xF4\x25\x00\x00' \
+                     b'\xFE\x37\x00\x00'
+        fp = DicomBytesIO(bytestream)
+        fp.is_little_endian = True
+        assert [0, 4966, 9716, 14334] == get_frame_offsets(fp)
+
+    def test_single_frame(self):
+        """Test reading single-frame BOT item"""
+        bytestream = b'\xFE\xFF\x00\xE0' \
+                     b'\x04\x00\x00\x00' \
+                     b'\x00\x00\x00\x00'  # Offset 1: 0
+        fp = DicomBytesIO(bytestream)
+        fp.is_little_endian = True
+        assert [0] == get_frame_offsets(fp)
 
 
-class TestReadItem(object):
-    """Test encaps.read_item"""
+class TestGetPixelDataFrames(object):
+    """Test encaps.get_pixel_data_frames"""
     pass
