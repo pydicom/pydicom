060ec24a5d8dc9b184e2fa2fced342b97b4d8f28
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-16 13:47:48 +0200

    iter

--- examples/input_output/plot_read_rtplan.py
+++ examples/input_output/plot_read_rtplan.py
@@ -24,7 +24,7 @@ def list_beams(plan_dataset):
         name="Beam name", num="Number", gantry="Gantry", ssd="SSD (cm)")]
     for beam in plan_dataset.BeamSequence:
         cp0 = beam.ControlPointSequence[0]
-        SSD = float(cp0.SourcetoSurfaceDistance / 10)
+        SSD = float(cp0.SourceToSurfaceDistance / 10)
         lines.append("{b.BeamName:^13s} {b.BeamNumber:8d} "
                      "{gantry:8.1f} {ssd:8.1f}".format(b=beam,
                                                        gantry=cp0.GantryAngle,
