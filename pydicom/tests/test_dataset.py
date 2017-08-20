af95d49ee06d47c6578b0055f852f83e9a94186b
scaramallion <scaramallion@users.noreply.github.com>
2017-08-20 23:57:13 +1000

    Fix dir(Dataset) not working correctly for subclasses (#487)
    * Add failing unit test for #486
    * Add check for Dataset method

--- pydicom/tests/test_dataset.py
+++ pydicom/tests/test_dataset.py
@@ -313,6 +313,22 @@ class DatasetTests(unittest.TestCase):
             'Johnny',
             "set by tag failed")
 
+    def test_dir_subclass(self):
+        """Dataset.__dir__ returns class specific dir"""
+        class DSP(Dataset):
+            def test_func(self):
+                pass
+
+        ds = DSP()
+        assert hasattr(ds, 'test_func')
+        assert callable(ds.test_func)
+        assert 'test_func' in dir(ds)
+
+        ds = Dataset()
+        assert hasattr(ds, 'group_dataset')
+        assert callable(ds.group_dataset)
+        assert 'group_dataset' in dir(ds)
+
     def test_dir(self):
         """Dataset.dir() returns sorted list of named data_elements."""
         ds = self.dummy_dataset()
