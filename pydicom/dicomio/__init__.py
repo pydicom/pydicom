589fb802b8fca412d3cc150e8d24593f0fb8c8fb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-23 16:43:53 +0200

    EHN/DOC add dcmread and docstring

--- pydicom/dicomio/__init__.py
+++ pydicom/dicomio/__init__.py
@@ -1,7 +1,9 @@
 """Input-output pydicom module"""
 
+from .filereader import dcmread
 from .filereader import read_file
 from .filereader import read_file_meta_info
+from .filereader import read_dicomdir
 from .filereader import read_partial
 from .filereader import read_preamble
 from .filereader import read_dataset
@@ -10,8 +12,14 @@ from .filewriter import write_file
 from .filewriter import write_file_meta_info
 from .filewriter import write_dataset
 
-__all__ = ['read_file',
+# create some aliases
+dcmwrite = write_file
+
+__all__ = ['dcmread',
+           'dcmwrite',
+           'read_file',
            'read_file_meta_info',
+           'read_dicomdir',
            'read_partial',
            'read_preamble',
            'read_dataset',
