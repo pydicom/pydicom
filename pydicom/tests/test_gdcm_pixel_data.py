0534df0e88563fec53d2de7d6bd975612e5fe833
rhaxton <robert.haxton@gmail.com>
2018-01-23 16:26:40 -0800

    See #521 - manually added convert yuv to rgb in gdcm tests

--- pydicom/tests/test_gdcm_pixel_data.py
+++ pydicom/tests/test_gdcm_pixel_data.py
@@ -341,6 +341,7 @@ class GDCM_JPEGlossyTests_with_gdcm(unittest.TestCase):
         self.assertEqual(self.color_3d_jpeg.PhotometricInterpretation, "YBR_FULL_422")
         a = self.color_3d_jpeg.pixel_array
         self.assertEqual(a.shape, (120, 480, 640, 3))
+        a = self.color_3d_jpeg._convert_YBR_to_RGB(a)
         # this test points were manually identified in Osirix viewer
         self.assertEqual(tuple(a[3, 159, 290, :]), (41, 41, 41))
         self.assertEqual(tuple(a[3, 169, 290, :]), (57, 57, 57))
@@ -375,7 +376,7 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ], ),
+     ], False),
     (sc_rgb_jpeg_dcmtk_411_YBR_FULL, "YBR_FULL",
      [
          (248, 3, 2),
@@ -388,7 +389,7 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ], ),
+     ], True),
     (sc_rgb_jpeg_dcmtk_411_YBR_FULL_422, "YBR_FULL_422",
      [
          (248, 3, 2),
@@ -401,7 +402,7 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ], ),
+     ], True),
     (sc_rgb_jpeg_dcmtk_422_YBR_FULL, "YBR_FULL",
      [
          (254, 0, 0),
@@ -414,7 +415,7 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ],),
+     ], True),
     (sc_rgb_jpeg_dcmtk_422_YBR_FULL_422, "YBR_FULL_422",
      [
          (254, 0, 0),
@@ -427,7 +428,7 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ], ),
+     ], True),
     (sc_rgb_jpeg_dcmtk_444_YBR_FULL, "YBR_FULL",
      [
          (254, 0, 0),
@@ -440,21 +441,27 @@ testdata = [
          (64, 64, 64),
          (192, 192, 192),
          (255, 255, 255),
-     ], ), ]
+     ], True), ]
 
 
 @pytest.mark.skipif(
     not test_gdcm_decoder,
     reason=gdcm_missing_message)
 @pytest.mark.parametrize(
-    "image,PhotometricInterpretation,results",
+    "image,PhotometricInterpretation,results,convert_yuv_to_rgb",
     testdata,
     ids=test_ids)
-def test_PI_RGB(test_with_gdcm, image, PhotometricInterpretation, results):
+def test_PI_RGB(test_with_gdcm,
+                image,
+                PhotometricInterpretation,
+                results,
+                convert_yuv_to_rgb):
     t = dcmread(image)
     assert t.PhotometricInterpretation == PhotometricInterpretation
     a = t.pixel_array
     assert a.shape == (100, 100, 3)
+    if convert_yuv_to_rgb:
+        a = t.color_3d_jpeg._convert_YBR_to_RGB(a)
     # this test points are from the ImageComments tag
     assert tuple(a[5, 50, :]) == results[0]
     assert tuple(a[15, 50, :]) == results[1]
