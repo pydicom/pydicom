85c821a6d9e5e00e26010ea4fb2b5387ee794b25
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-17 22:37:05 +0100

    iter

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ information such as reports and radiotherapy objects.
 pydicom makes it easy to read these complex files into natural pythonic
 structures for easy manipulation.  Modified datasets can be written again to
 DICOM format files. See the `Getting Started
-<https://pydicom.giathub.io/pydicom/stable/getting_started.html>`_ page for
+<https://pydicom.github.io/pydicom/stable/getting_started.html>`_ page for
 installation and basic information, and the `Pydicom User Guide
-<https://pydicom.giathub.io/pydicom/stable/getting_started.html>`_ page for
+<https://pydicom.github.io/pydicom/stable/getting_started.html>`_ page for
 an overview of how to use the pydicom library.
 """
 
