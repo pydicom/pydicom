3f0017ca3c09aed083219665089cbd2f3ef0878a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-19 10:27:29 +0100

    FIX infer __version_info__ from __version__

--- pydicom/_version.py
+++ pydicom/_version.py
@@ -1,4 +1,4 @@
 """Pure python package for DICOM medical file reading and writing."""
 
 __version__ = '1.1.0.dev0'
-__version_info__ = (1, 1, 0)
+__version_info__ = tuple(__version__.split('.')[:3])
