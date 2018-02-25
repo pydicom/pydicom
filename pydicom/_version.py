898c0cfda7153e24514a7f1bbc346392e62484e7
Darcy Mason <darcymason@gmail.com>
2018-02-24 21:27:14 -0500

    Update version to 1.0.1 due to previous 1.0.0 on PyPi

--- pydicom/_version.py
+++ pydicom/_version.py
@@ -1,6 +1,6 @@
 """Pure python package for DICOM medical file reading and writing."""
 import re
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __version_info__ = tuple(
     re.match(r'(\d+\.\d+\.\d+).*', __version__).group(1).split('.'))
