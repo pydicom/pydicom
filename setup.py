762ec11d6104107734fd1e45bd1fd966e0ea2ac4
Yaroslav Halchenko <debian@onerussian.com>
2018-02-27 11:42:14 -0500

    pep8, space

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 # get __version__ from _version.py
 base_dir = os.path.dirname(os.path.realpath(__file__))
-ver_file = os.path.join(base_dir,'pydicom', '_version.py')
+ver_file = os.path.join(base_dir, 'pydicom', '_version.py')
 with open(ver_file) as f:
     exec(f.read())
 
