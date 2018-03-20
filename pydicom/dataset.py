958ef9488d390be1cefe383fd6c409ce949da0c7
Amos Onn <amos.onn@sp-institute.com>
2018-03-20 14:50:28 +0100

    Fixed Dataset.copy.
    Method inherited from dict is broken.

--- pydicom/dataset.py
+++ pydicom/dataset.py
@@ -1131,6 +1131,13 @@ class Dataset(dict):
                     for elem in dataset.iterall():
                         yield elem
 
+    def copy(self):
+        """Copy the entire dataset.
+
+        Overrides method inherited from dict.
+        """
+        return self[:]
+
     def walk(self, callback, recursive=True):
         """Iterate through the DataElements and run `callback` on each.
 
