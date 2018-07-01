43117d40a6be62a7b1273bd815366208113a2fd6
Darcy Mason <darcymason@gmail.com>
2018-07-01 10:52:06 -0400

    Update version to 1.2.X series and remove bumpversion config (#661)
    * Update version to 1.2.X series and remove bumpversion config
    
    * Updated docs for version change (removes bumpversion references)
    
    * Removes dev guide as all info in wiki

--- pydicom/_version.py
+++ pydicom/_version.py
@@ -1,6 +1,6 @@
 """Pure python package for DICOM medical file reading and writing."""
 import re
 
-__version__ = '1.1.0'
+__version__ = '1.2.0.dev0'
 __version_info__ = tuple(
     re.match(r'(\d+\.\d+\.\d+).*', __version__).group(1).split('.'))
