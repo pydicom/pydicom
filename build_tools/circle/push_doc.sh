6a1d5cb4e182e99efd7f7d0658851af8dd4eade6
scaramallion <scaramallion@users.noreply.github.com>
2018-07-13 17:17:00 +1000

    Final update

--- build_tools/circle/push_doc.sh
+++ build_tools/circle/push_doc.sh
@@ -11,8 +11,8 @@
 #   If the git branch is anything else then we just want to test that committing
 #       the changes works so that any issues can be debugged
 
-function git_fetch_commit {
-    # Clone the pydicom repo, then checkout gh-pages, update the $DIR directory
+function doc_clone_commit {
+    # Clone the pydicom/$DOC_BRANCH branch, update the $DIR directory
     #   by deleting existing content and copying the most recent version from the
     #   $CIRCLE_BRANCH, then commit the changes with message $MSG
     MSG="Updating the docs in $DIR/ for branch: $CIRCLE_BRANCH, commit $CIRCLE_SHA1"
@@ -26,12 +26,11 @@ function git_fetch_commit {
     cd $HOME
     if [ ! -d $CIRCLE_PROJECT_REPONAME ]
     then
-        git clone -b gh-pages --single-branch $CIRCLE_REPOSITORY_URL
+        git clone -b $DOC_BRANCH --single-branch $CIRCLE_REPOSITORY_URL
     fi
 
     cd $CIRCLE_PROJECT_REPONAME
-    git reset --hard origin/gh-pages
-    # Remove the existing document subdirectory and copy across the current version
+    git reset --hard origin/$DOC_BRANCH
     git rm -rf $DIR/ && rm -rf $DIR/
     cp -R $HOME/project/doc/_build/html $DIR
     git config --global user.email $EMAIL
@@ -50,31 +49,33 @@ if [ -z ${HOME+x} ]; then echo "HOME is unset"; fi
 if [ -z ${EMAIL+x} ]; then echo "EMAIL is unset"; fi
 if [ -z ${USERNAME+x} ]; then echo "USERNAME is unset"; fi
 
+DOC_BRANCH=gh-pages
+
 # Determine which of the three workflows to take
 if [ "$CIRCLE_BRANCH" = "master" ]
 then
     # build of current master
-    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/gh-pages for $CIRCLE_BRANCH"
+    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
     # Changes are made to dev/ directory
     DIR=dev
-    git_fetch_commit
-    git push origin gh-pages
+    doc_clone_commit
+    git push origin $DOC_BRANCH
     echo "Push complete"
 elif [[ "$CIRCLE_BRANCH" =~ ^[0-9]+\.[0-9]+\.X$ ]]
 then
     # build of release, matches branch name against 0.1.X, 91.235.X, etc
-    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/gh-pages for $CIRCLE_BRANCH"
+    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
     # Strip off .X from branch name, so changes will go to 0.1/, 91.235/, etc
     DIR="${CIRCLE_BRANCH::-2}"
-    git_fetch_commit
-    git push origin gh-pages
+    doc_clone_commit
+    git push origin $DOC_BRANCH
     echo "Push complete"
 else
     # build pull release, should be regex ^pull\/[0-9]+$ but lets run against
     #   everything that doesn't match the other two workflows
-    echo "Testing commit only to $CIRCLE_PROJECT_REPONAME/gh-pages for $CIRCLE_BRANCH"
+    echo "Testing commit only to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
     # Changes are made to dev/ directory but not merged
     DIR=dev
-    git_fetch_commit
+    doc_clone_commit
     echo "Test complete, changes NOT pushed"
 fi
