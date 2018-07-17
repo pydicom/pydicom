ded55674279792fa25597e3ceea28268e2ffb488
scaramallion <scaramallion@users.noreply.github.com>
2018-07-17 17:08:15 +1000

    Undo overzealous deletion

--- pydicom/compat.py
+++ pydicom/compat.py
@@ -1,6 +1,9 @@
 # Copyright 2008-2018 pydicom authors. See LICENSE file for details.
 """Compatibility functions for python 2 vs later versions"""
 
+# These are largely modeled on Armin Ronacher's porting advice
+-# at http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/
+
 import sys
 
 in_py2 = sys.version_info[0] == 2
--- pydicom/dicomio.py
+++ pydicom/dicomio.py
@@ -1,4 +1,5 @@
 # Copyright 2008-2018 pydicom authors. See LICENSE file for details.
 """Many point of entry for pydicom read and write functions"""
+
 from pydicom.filereader import (dcmread, read_file, read_dicomdir)
 from pydicom.filewriter import dcmwrite, write_file
