3e22eb0cd2b9d71224d66eba1e87012b29651070
scaramallion <scaramallion@users.noreply.github.com>
2018-07-17 17:08:52 +1000

    Fix extra character

--- pydicom/compat.py
+++ pydicom/compat.py
@@ -2,7 +2,7 @@
 """Compatibility functions for python 2 vs later versions"""
 
 # These are largely modeled on Armin Ronacher's porting advice
--# at http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/
+# at http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/
 
 import sys
 
