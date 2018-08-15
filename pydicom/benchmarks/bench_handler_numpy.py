27e38e626a8740b2b10054e055317774fdcb5d33
scaramallion <scaramallion@users.noreply.github.com>
2018-08-15 13:00:01 +1000

    Add benchmarks

--- pydicom/benchmarks/bench_handler_numpy.py
+++ pydicom/benchmarks/bench_handler_numpy.py
@@ -1,2 +1,129 @@
 # Copyright 2008-2018 pydicom authors. See LICENSE file for details.
 """Benchmarks for the numpy_handler module."""
+
+from pydicom import dcmread
+from pydicom.data import get_testdata_files
+from pydicom.encaps import decode_data_sequence
+from pydicom.pixel_data_handlers.numpy_handler import get_pixeldata
+
+
+# 1/1, 1 sample/pixel, 1 frame
+EXPL_1_1_1F = get_testdata_files("liver_1frame.dcm")[0]
+# 1/1, 1 sample/pixel, 3 frame
+EXPL_1_1_3F = get_testdata_files("liver.dcm")[0]
+# 8/8, 1 sample/pixel, 1 frame
+EXPL_8_1_1F = get_testdata_files("OBXXXX1A.dcm")[0]
+# 8/8, 1 sample/pixel, 2 frame
+EXPL_8_1_2F = get_testdata_files("OBXXXX1A_2frame.dcm")[0]
+# 8/8, 3 sample/pixel, 1 frame
+EXPL_8_3_1F = get_testdata_files("SC_rgb.dcm")[0]
+# 8/8, 3 sample/pixel, 2 frame
+EXPL_8_3_2F = get_testdata_files("SC_rgb_2frame.dcm")[0]
+# 16/16, 1 sample/pixel, 1 frame
+EXPL_16_1_1F = get_testdata_files("MR_small.dcm")[0]
+# 16/12, 1 sample/pixel, 10 frame
+EXPL_16_1_10F = get_testdata_files("emri_small.dcm")[0]
+# 16/16, 3 sample/pixel, 1 frame
+EXPL_16_3_1F = get_testdata_files("SC_rgb_16bit.dcm")[0]
+# 16/16, 3 sample/pixel, 2 frame
+EXPL_16_3_2F = get_testdata_files("SC_rgb_16bit_2frame.dcm")[0]
+# 32/32, 1 sample/pixel, 1 frame
+IMPL_32_1_1F = get_testdata_files("rtdose_1frame.dcm")[0]
+# 32/32, 1 sample/pixel, 15 frame
+IMPL_32_1_15F = get_testdata_files("rtdose.dcm")[0]
+# 32/32, 3 sample/pixel, 1 frame
+EXPL_32_3_1F = get_testdata_files("SC_rgb_32bit.dcm")[0]
+# 32/32, 3 sample/pixel, 2 frame
+EXPL_32_3_2F = get_testdata_files("SC_rgb_32bit_2frame.dcm")[0]
+
+
+class TimeGetPixelData(object):
+    """Time tests for numpy_handler.get_pixeldata."""
+    def setup(self):
+        """Setup the tests."""
+        self.no_runs = 100
+
+        self.ds_1_1_1 = dcmread(EXPL_1_1_1F)
+        self.ds_1_1_3 = dcmread(EXPL_1_1_3F)
+        self.ds_8_1_1 = dcmread(EXPL_8_1_1F)
+        self.ds_8_1_2 = dcmread(EXPL_8_1_2F)
+        self.ds_8_3_1 = dcmread(EXPL_8_3_1F)
+        self.ds_8_3_2 = dcmread(EXPL_8_3_2F)
+        self.ds_16_1_1 = dcmread(EXPL_16_1_1F)
+        self.ds_16_1_10 = dcmread(EXPL_16_1_10F)
+        self.ds_16_3_1 = dcmread(EXPL_16_3_1F)
+        self.ds_16_3_2 = dcmread(EXPL_16_3_2F)
+        self.ds_32_1_1 = dcmread(IMPL_32_1_1F)
+        self.ds_32_1_15 = dcmread(IMPL_32_1_15F)
+        self.ds_32_3_1 = dcmread(EXPL_32_3_1F)
+        self.ds_32_3_2 = dcmread(EXPL_32_3_2F)
+
+    def time_1bit_1sample_1frame(self):
+        """Time retrieval of 1-bit, 1 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_1_1_1)
+
+    def time_1bit_1sample_3frame(self):
+        """Time retrieval of 1-bit, 1 sample/pixel, 3 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_1_1_3)
+
+    def time_8bit_1sample_1frame(self):
+        """Time retrieval of 8-bit, 1 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_8_1_1)
+
+    def time_8bit_1sample_2frame(self):
+        """Time retrieval of 8-bit, 1 sample/pixel, 2 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_8_1_2)
+
+    def time_8bit_3sample_1frame(self):
+        """Time retrieval of 8-bit, 3 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_8_3_1)
+
+    def time_8bit_3sample_2frame(self):
+        """Time retrieval of 8-bit, 3 sample/pixel, 2 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_8_3_2)
+
+    def time_16bit_1sample_1frame(self):
+        """Time retrieval of 16-bit, 1 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_16_1_1)
+
+    def time_16bit_1sample_10frame(self):
+        """Time retrieval of 16-bit, 1 sample/pixel, 10 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_16_1_10)
+
+    def time_16bit_3sample_1frame(self):
+        """Time retrieval of 16-bit, 3 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_16_3_1)
+
+    def time_16bit_3sample_2frame(self):
+        """Time retrieval of 16-bit, 3 sample/pixel, 2 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_16_3_2)
+
+    def time_32bit_1sample_1frame(self):
+        """Time retrieval of 32-bit, 1 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_32_1_1)
+
+    def time_32bit_1sample_15frame(self):
+        """Time retrieval of 32-bit, 1 sample/pixel, 15 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_32_1_15)
+
+    def time_32bit_3sample_1frame(self):
+        """Time retrieval of 32-bit, 3 sample/pixel, 1 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_32_3_1)
+
+    def time_32bit_3sample_2frame(self):
+        """Time retrieval of 32-bit, 3 sample/pixel, 2 frame."""
+        for ii in range(self.no_runs):
+            get_pixeldata(self.ds_32_3_2)
