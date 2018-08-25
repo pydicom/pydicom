Fix YBR to RGB conversion overflow, add tests, add convert_colour_space, add RGB to YBR conversion

pydicom/pixel_data_handlers/util.py
@@ -14,6 +14,52 @@ except ImportError:
 sys_is_little_endian = (sys.byteorder == 'little')
 
 
+def convert_colour_space(arr, current, desired):
+    """Convert the image(s) in `arr` from one colour space to another.
+
+    Parameters
+    ----------
+    arr : numpy.ndarray
+        The image(s) as an ndarray with shape (frames, rows, columns)
+        or (rows, columns).
+    current : str
+        The current colour space, should be a valid value for (0028,0004)
+        *Photometric Interpretation*. One of 'RGB', 'YBR_FULL'.
+    desired : str
+        The desired colour space, should be a valid value for (0028,0004)
+        *Photometric Interpretation*. One of 'RGB', 'YBR_FULL'.
+
+    Returns
+    -------
+    numpy.ndarray
+        The image(s) converted to the desired colour space.
+    """
+    # No change needed
+    if current == desired:
+        return arr
+
+    _converters = {
+        'YBR_FULL' : {
+            'RGB' : _convert_YBR_FULL_to_RGB
+        },
+        'RGB' : {
+            'YBR_FULL' : _convert_RGB_to_YBR_FULL
+        }
+    }
+    try:
+        converter = _converters[current][desired]
+    except KeyError:
+        raise NotImplementedError(
+            "Conversion from {0} to {1} is not supported."
+            .format(current, desired)
+        )
+
+    return converter(arr)
+
+
+convert_color_space = convert_colour_space
+
+
 def dtype_corrected_for_endianess(is_little_endian, numpy_dtype):
     """Adapts the given numpy data type for changing the endianess of the
     dataset, if needed.
@@ -45,6 +91,93 @@ def dtype_corrected_for_endianess(is_little_endian, numpy_dtype):
         return numpy_dtype.newbyteorder('S')
 
     return numpy_dtype
+
+
+def pixel_dtype(ds):
+    """Return a numpy dtype for the pixel data in dataset in `ds`.
+
+    Suitable for use with IODs containing the Image Pixel module.
+
+    +------------------------------------------+--------------+
+    | Element                                  | Supported    |
+    +-------------+---------------------+------+ values       |
+    | Tag         | Keyword             | Type |              |
+    +=============+=====================+======+==============+
+    | (0028,0101) | BitsAllocated       | 1    | 1, 8, 16, 32 |
+    +-------------+---------------------+------+--------------+
+    | (0028,0103) | PixelRepresentation | 1    | 0, 1         |
+    +-------------+---------------------+------+--------------+
+
+    Parameters
+    ----------
+    ds : dataset.Dataset
+        The DICOM dataset containing the pixel data you wish to get the
+        numpy dtype for.
+
+    Returns
+    -------
+    numpy.dtype
+        A numpy dtype suitable for containing the dataset's pixel data.
+
+    Raises
+    ------
+    NotImplementedError
+        If the pixel data is of a type that isn't supported by either numpy
+        or pydicom.
+    """
+    if not HAVE_NP:
+        raise ImportError("Numpy is required to determine the dtype.")
+
+    if ds.is_little_endian is None:
+        ds.is_little_endian = ds.file_meta.TransferSyntaxUID.is_little_endian
+
+    # (0028,0103) Pixel Representation, US, 1
+    #   Data representation of the pixel samples
+    #   0x0000 - unsigned int
+    #   0x0001 - 2's complement (signed int)
+    pixel_repr = ds.PixelRepresentation
+    if pixel_repr == 0:
+        dtype_str = 'uint'
+    elif pixel_repr == 1:
+        dtype_str = 'int'
+    else:
+        raise NotImplementedError(
+            "Unable to determine the data type to use to contain the "
+            "Pixel Data as a value of '{}' for '(0028,0103) Pixel "
+            "Representation' is not supported".format(pixel_repr)
+        )
+
+    # (0028,0100) Bits Allocated, US, 1
+    #   The number of bits allocated for each pixel sample
+    #   PS3.5 8.1.1: Bits Allocated shall either be 1 or a multiple of 8
+    #   For bit packed data we use uint8
+    bits_allocated = ds.BitsAllocated
+    if bits_allocated == 1:
+        dtype_str = 'uint8'
+    elif bits_allocated > 0 and bits_allocated % 8 == 0:
+        dtype_str += str(bits_allocated)
+    else:
+        raise NotImplementedError(
+            "Unable to determine the data type to use to contain the "
+            "Pixel Data as a value of '{}' for '(0028,0100) Bits "
+            "Allocated' is not supported".format(bits_allocated)
+        )
+
+    # Check to see if the dtype is valid for numpy
+    try:
+        dtype = np.dtype(dtype_str)
+    except TypeError:
+        raise NotImplementedError(
+            "The data type '{}' needed to contain the Pixel Data is not "
+            "supported by numpy".format(dtype_str)
+        )
+
+    # Correct for endianness of the system vs endianness of the dataset
+    if ds.is_little_endian != (byteorder == 'little'):
+        # 'S' swap from current to opposite
+        dtype = dtype.newbyteorder('S')
+
+    return dtype
 
 
 def reshape_pixel_array(ds, arr):
@@ -193,124 +326,101 @@ def reshape_pixel_array(ds, arr):
     return arr
 
 
-def convert_YBR_to_RGB(arr_ybr):
-    """Return an ndarray converted from YCbCr to RGB colour space.
+def _convert_RGB_to_YBR_FULL(arr):
+    """Return an ndarray converted from RGB to YBR_FULL colour space.
 
     Parameters
     ----------
-    arr_ybr : numpy.ndarray
-        An ndarray of an image in YCbCr (luminance, chrominance) space.
+    arr : numpy.ndarray
+        An ndarray of an 8-bit per channel image in RGB colour space.
 
     Returns
     -------
     numpy.ndarray
-        The ndarray in RGB colour space.
+        The image in YBR_FULL colour space.
+
+    References
+    ----------
+
+    * DICOM Standard, Part 3, Annex C.7.6.3.1.2
+    * ISO/IEC 10918-5:2012, Section 7
     """
     if not HAVE_NP:
         raise ImportError(
             "Numpy is required to convert the color space."
         )
 
-    orig_dtype = arr_ybr.dtype
+    orig_dtype = arr.dtype
+    arr = arr.astype(np.float)
 
-    # Conversion from PhotometricInterpretation of YBR to RGB
-    #   PS3.3 C.7.6.3.1.2
-    # https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion
-    ybr_to_rgb = np.asarray(
-        [[1.0, +0.000000, +1.402000],
-         [1.0, -0.344136, -0.714136],
-         [1.0, +1.772000, +0.000000]], dtype=np.float)
+    rgb_to_ybr = np.asarray(
+        [[+0.299, +0.587, +0.114],
+         [-0.299, -0.587, +0.886],
+         [+0.701, -0.587, -0.114]], dtype=np.float)
 
-    arr_ybr = arr_ybr.astype(np.float)
-    arr_ybr -= [0, 128, 128]
-    # Why copy?
-    arr_ybr = np.dot(arr_ybr, ybr_to_rgb.T.copy())
+    arr = np.dot(arr, rgb_to_ybr.T)
+    if len(arr.shape) == 4:
+        # Multi-frame
+        arr[:, :, :, 1] /= 1.772
+        arr[:, :, :, 2] /= 1.402
+    else:
+        # Single frame
+        arr[:, :, 1] /= 1.772
+        arr[:, :, 2] /= 1.402
 
-    return arr_ybr.astype(orig_dtype)
+    arr += [0, 128, 128]
+
+    # Round(x) -> floor of (arr + 0.5)
+    arr = np.floor(arr + 0.5)
+    # Max(0, arr) -> 0 if 0 >= arr, arr otherwise
+    arr[np.where(arr < 0)] = 0
+    # Min(arr, 255) -> arr if arr <= 255, 255 otherwise
+    arr[np.where(arr > 255)] = 255
+
+    return arr.astype(orig_dtype)
 
 
-def pixel_dtype(ds):
-    """Return a numpy dtype for the pixel data in dataset in `ds`.
-
-    Suitable for use with IODs containing the Image Pixel module.
-
-    +------------------------------------------+--------------+
-    | Element                                  | Supported    |
-    +-------------+---------------------+------+ values       |
-    | Tag         | Keyword             | Type |              |
-    +=============+=====================+======+==============+
-    | (0028,0101) | BitsAllocated       | 1    | 1, 8, 16, 32 |
-    +-------------+---------------------+------+--------------+
-    | (0028,0103) | PixelRepresentation | 1    | 0, 1         |
-    +-------------+---------------------+------+--------------+
+def _convert_YBR_FULL_to_RGB(arr):
+    """Return an ndarray converted from YBR_FULL to RGB colour space.
 
     Parameters
     ----------
-    ds : dataset.Dataset
-        The DICOM dataset containing the pixel data you wish to get the
-        numpy dtype for.
+    arr : numpy.ndarray
+        An ndarray of an 8-bit per channel image in YBR_FULL colour space.
 
     Returns
     -------
-    numpy.dtype
-        A numpy dtype suitable for containing the dataset's pixel data.
+    numpy.ndarray
+        The image in RGB colour space.
 
-    Raises
-    ------
-    NotImplementedError
-        If the pixel data is of a type that isn't supported by either numpy
-        or pydicom.
+    References
+    ----------
+
+    * DICOM Standard, Part 3, Annex C.7.6.3.1.2
+    * ISO/IEC 10918-5:2012, Section 7
     """
     if not HAVE_NP:
-        raise ImportError("Numpy is required to determine the dtype.")
-
-    if ds.is_little_endian is None:
-        ds.is_little_endian = ds.file_meta.TransferSyntaxUID.is_little_endian
-
-    # (0028,0103) Pixel Representation, US, 1
-    #   Data representation of the pixel samples
-    #   0x0000 - unsigned int
-    #   0x0001 - 2's complement (signed int)
-    pixel_repr = ds.PixelRepresentation
-    if pixel_repr == 0:
-        dtype_str = 'uint'
-    elif pixel_repr == 1:
-        dtype_str = 'int'
-    else:
-        raise NotImplementedError(
-            "Unable to determine the data type to use to contain the "
-            "Pixel Data as a value of '{}' for '(0028,0103) Pixel "
-            "Representation' is not supported".format(pixel_repr)
+        raise ImportError(
+            "Numpy is required to convert the color space."
         )
 
-    # (0028,0100) Bits Allocated, US, 1
-    #   The number of bits allocated for each pixel sample
-    #   PS3.5 8.1.1: Bits Allocated shall either be 1 or a multiple of 8
-    #   For bit packed data we use uint8
-    bits_allocated = ds.BitsAllocated
-    if bits_allocated == 1:
-        dtype_str = 'uint8'
-    elif bits_allocated > 0 and bits_allocated % 8 == 0:
-        dtype_str += str(bits_allocated)
-    else:
-        raise NotImplementedError(
-            "Unable to determine the data type to use to contain the "
-            "Pixel Data as a value of '{}' for '(0028,0100) Bits "
-            "Allocated' is not supported".format(bits_allocated)
-        )
+    orig_dtype = arr.dtype
 
-    # Check to see if the dtype is valid for numpy
-    try:
-        dtype = np.dtype(dtype_str)
-    except TypeError:
-        raise NotImplementedError(
-            "The data type '{}' needed to contain the Pixel Data is not "
-            "supported by numpy".format(dtype_str)
-        )
+    ybr_to_rgb = np.asarray(
+        [[1.0, +0.0, +1.402],
+         [1.0, -0.114 * 1.772 / 0.587, -0.299 * 1.402 / 0.587],
+         [1.0, +1.772, +0.0]], dtype=np.float)
 
-    # Correct for endianness of the system vs endianness of the dataset
-    if ds.is_little_endian != (byteorder == 'little'):
-        # 'S' swap from current to opposite
-        dtype = dtype.newbyteorder('S')
+    arr = arr.astype(np.float)
+    arr -= [0, 128, 128]
+    # Why copy?
+    arr = np.dot(arr, ybr_to_rgb.T)
 
-    return dtype
+    # Round(x) -> floor of (arr + 0.5)
+    arr = np.floor(arr + 0.5)
+    # Max(0, arr) -> 0 if 0 >= arr, arr otherwise
+    arr[np.where(arr < 0)] = 0
+    # Min(arr, 255) -> arr if arr <= 255, 255 otherwise
+    arr[np.where(arr > 255)] = 255
+
+    return arr.astype(orig_dtype)

pydicom/tests/test_handler_util.py
@@ -22,20 +22,31 @@ try:
 except ImportError:
     HAVE_NP = False
 
+from pydicom import dcmread
+from pydicom.data import get_testdata_files
 from pydicom.dataset import Dataset
 from pydicom.pixel_data_handlers.util import (
     dtype_corrected_for_endianess,
     reshape_pixel_array,
-    convert_YBR_to_RGB,
+    _convert_YBR_FULL_to_RGB,
+    _convert_RGB_to_YBR_FULL,
+    convert_colour_space,
     pixel_dtype
 )
 from pydicom.uid import (ExplicitVRLittleEndian,
                          UncompressedPixelTransferSyntaxes)
 
 
+# 8 bit, 3 samples/pixel, 1 and 2 frame datasets
+# RGB colourspace, uncompressed
+RGB_8_3_1F = get_testdata_files("SC_rgb.dcm")[0]
+RGB_8_3_2F = get_testdata_files("SC_rgb_2frame.dcm")[0]
+
+
 # Tests with Numpy unavailable
 @pytest.mark.skipif(HAVE_NP, reason='Numpy is available')
 class TestNoNumpy(object):
+    """Tests for the util functions without numpy."""
     def test_pixel_dtype_raises(self):
         """Test that pixel_dtype raises exception without numpy."""
         with pytest.raises(ImportError,
@@ -47,6 +58,18 @@ class TestNoNumpy(object):
         with pytest.raises(ImportError,
                            match="Numpy is required to reshape"):
             reshape_pixel_array(None, None)
+
+    def test_convert_YBRF_RGB_raises(self):
+        """Test that _convert_YBR_FULL_to_RGB raises exception."""
+        with pytest.raises(ImportError,
+                           match="Numpy is required to convert"):
+            convert_YBR_FULL_to_RGB(None)
+
+    def test_convert_RGB_YBRF_raises(self):
+        """Test that _convert_RGB_to_YBR_FULL raises exception."""
+        with pytest.raises(ImportError,
+                           match="Numpy is required to convert"):
+            convert_RGB_to_YBR_FULL(None)
 
 
 # Tests with Numpy available
@@ -61,7 +84,7 @@ REFERENCE_DTYPE = [
 
 @pytest.mark.skipif(not HAVE_NP, reason="Numpy is not available")
 class TestNumpy_PixelDtype(object):
-    """Tests for numpy_handler.pixel_dtype."""
+    """Tests for util.pixel_dtype."""
     def setup(self):
         """Setup the test dataset."""
         self.ds = Dataset()
@@ -246,7 +269,7 @@ if HAVE_NP:
 
 @pytest.mark.skipif(not HAVE_NP, reason="Numpy is not available")
 class TestNumpy_ReshapePixelArray(object):
-    """Tests for numpy_handler.pixel_dtype."""
+    """Tests for util.pixel_dtype."""
     def setup(self):
         """Setup the test dataset."""
         self.ds = Dataset()
@@ -494,3 +517,138 @@ class TestNumpy_ReshapePixelArray(object):
                            match="value of 2 for \(0028,0006\)"):
             reshape_pixel_array(self.ds,
                                 RESHAPE_ARRAYS['1frame_3sample_0config'])
+
+
+@pytest.mark.skipif(not HAVE_NP, reason="Numpy is not available")
+class TestNumpy_ConvertColourSpace(object):
+    """Tests for util.convert_colour_space."""
+    def setup(self):
+        """Setup the test dataset."""
+        self.blank_arr = np.ones((2, 3))
+
+    def test_unknown_current_raises(self):
+        """Test an unknown current colour space raises exception."""
+        with pytest.raises(NotImplementedError,
+                           match="Conversion from TEST to RGB is not suppo"):
+            convert_colour_space(None, 'TEST', 'RGB')
+
+    def test_unknown_desired_raises(self):
+        def test_unknown_current_raises(self):
+            """Test an unknown current colour space raises exception."""
+        with pytest.raises(NotImplementedError,
+                           match="Conversion from RGB to TEST is not suppo"):
+            convert_colour_space(None, 'RGB', 'TEST')
+
+    def test_current_is_desired(self):
+        """Test that the array is unchanged when current matches desired."""
+        assert np.array_equal(
+            self.blank_arr,
+            convert_colour_space(self.blank_arr, 'RGB', 'RGB')
+        )
+
+    # Test RGB to YBR_FULL
+    def test_single_frame(self):
+        """Test round trip conversion of single framed pixel data."""
+        ds = dcmread(RGB_8_3_1F)
+
+        arr = ds.pixel_array
+        assert (255, 0, 0) == tuple(arr[5, 50, :])
+        assert (255, 128, 128) == tuple(arr[15, 50, :])
+        assert (0, 255, 0) == tuple(arr[25, 50, :])
+        assert (128, 255, 128) == tuple(arr[35, 50, :])
+        assert (0, 0, 255) == tuple(arr[45, 50, :])
+        assert (128, 128, 255) == tuple(arr[55, 50, :])
+        assert (0, 0, 0) == tuple(arr[65, 50, :])
+        assert (64, 64, 64) == tuple(arr[75, 50, :])
+        assert (192, 192, 192) == tuple(arr[85, 50, :])
+        assert (255, 255, 255) == tuple(arr[95, 50, :])
+
+        ybr = convert_colour_space(arr, 'RGB', 'YBR_FULL')
+        assert (76, 85, 255) == tuple(ybr[5, 50, :])
+        assert (166, 107, 192) == tuple(ybr[15, 50, :])
+        assert (150, 44, 21) == tuple(ybr[25, 50, :])
+        assert (203, 86, 75) == tuple(ybr[35, 50, :])
+        assert (29, 255, 107) == tuple(ybr[45, 50, :])
+        assert (142, 192, 118) == tuple(ybr[55, 50, :])
+        assert (0, 128, 128) == tuple(ybr[65, 50, :])
+        assert (64, 128, 128) == tuple(ybr[75, 50, :])
+        assert (192, 128, 128) == tuple(ybr[85, 50, :])
+        assert (255, 128, 128) == tuple(ybr[95, 50, :])
+
+        # Round trip -> rounding errors get compounded
+        rgb = convert_colour_space(ybr, 'YBR_FULL', 'RGB')
+        assert (254, 0, 0) == tuple(rgb[5, 50, :])
+        assert (255, 128, 129) == tuple(rgb[15, 50, :])
+        assert (0, 255, 1) == tuple(rgb[25, 50, :])
+        assert (129, 255, 129) == tuple(rgb[35, 50, :])
+        assert (0, 0, 254) == tuple(rgb[45, 50, :])
+        assert (128, 127, 255) == tuple(rgb[55, 50, :])
+        assert (0, 0, 0) == tuple(rgb[65, 50, :])
+        assert (64, 64, 64) == tuple(rgb[75, 50, :])
+        assert (192, 192, 192) == tuple(rgb[85, 50, :])
+        assert (255, 255, 255) == tuple(rgb[95, 50, :])
+
+    def test_multi_frame(self):
+        """Test round trip conversion of multi-framed pixel data."""
+        ds = dcmread(RGB_8_3_2F)
+
+        arr = ds.pixel_array
+        assert (255, 0, 0) == tuple(arr[0, 5, 50, :])
+        assert (255, 128, 128) == tuple(arr[0, 15, 50, :])
+        assert (0, 255, 0) == tuple(arr[0, 25, 50, :])
+        assert (128, 255, 128) == tuple(arr[0, 35, 50, :])
+        assert (0, 0, 255) == tuple(arr[0, 45, 50, :])
+        assert (128, 128, 255) == tuple(arr[0, 55, 50, :])
+        assert (0, 0, 0) == tuple(arr[0, 65, 50, :])
+        assert (64, 64, 64) == tuple(arr[0, 75, 50, :])
+        assert (192, 192, 192) == tuple(arr[0, 85, 50, :])
+        assert (255, 255, 255) == tuple(arr[0, 95, 50, :])
+        # Frame 2 is frame 1 inverted
+        assert np.array_equal((2**ds.BitsAllocated - 1) - arr[1], arr[0])
+
+        ybr = convert_colour_space(arr, 'RGB', 'YBR_FULL')
+        assert (76, 85, 255) == tuple(ybr[0, 5, 50, :])
+        assert (166, 107, 192) == tuple(ybr[0, 15, 50, :])
+        assert (150, 44, 21) == tuple(ybr[0, 25, 50, :])
+        assert (203, 86, 75) == tuple(ybr[0, 35, 50, :])
+        assert (29, 255, 107) == tuple(ybr[0, 45, 50, :])
+        assert (142, 192, 118) == tuple(ybr[0, 55, 50, :])
+        assert (0, 128, 128) == tuple(ybr[0, 65, 50, :])
+        assert (64, 128, 128) == tuple(ybr[0, 75, 50, :])
+        assert (192, 128, 128) == tuple(ybr[0, 85, 50, :])
+        assert (255, 128, 128) == tuple(ybr[0, 95, 50, :])
+        # Frame 2
+        assert (179, 171, 1) == tuple(ybr[1, 5, 50, :])
+        assert (89, 149, 65) == tuple(ybr[1, 15, 50, :])
+        assert (105, 212, 235) == tuple(ybr[1, 25, 50, :])
+        assert (52, 170, 181) == tuple(ybr[1, 35, 50, :])
+        assert (226, 1, 149) == tuple(ybr[1, 45, 50, :])
+        assert (113, 65, 138) == tuple(ybr[1, 55, 50, :])
+        assert (255, 128, 128) == tuple(ybr[1, 65, 50, :])
+        assert (191, 128, 128) == tuple(ybr[1, 75, 50, :])
+        assert (63, 128, 128) == tuple(ybr[1, 85, 50, :])
+        assert (0, 128, 128) == tuple(ybr[1, 95, 50, :])
+
+        # Round trip -> rounding errors get compounded
+        rgb = convert_colour_space(ybr, 'YBR_FULL', 'RGB')
+        assert (254, 0, 0) == tuple(rgb[0, 5, 50, :])
+        assert (255, 128, 129) == tuple(rgb[0, 15, 50, :])
+        assert (0, 255, 1) == tuple(rgb[0, 25, 50, :])
+        assert (129, 255, 129) == tuple(rgb[0, 35, 50, :])
+        assert (0, 0, 254) == tuple(rgb[0, 45, 50, :])
+        assert (128, 127, 255) == tuple(rgb[0, 55, 50, :])
+        assert (0, 0, 0) == tuple(rgb[0, 65, 50, :])
+        assert (64, 64, 64) == tuple(rgb[0, 75, 50, :])
+        assert (192, 192, 192) == tuple(rgb[0, 85, 50, :])
+        assert (255, 255, 255) == tuple(rgb[0, 95, 50, :])
+        # Frame 2
+        assert (1, 255, 255) == tuple(rgb[1, 5, 50, :])
+        assert (1, 127, 126) == tuple(rgb[1, 15, 50, :])
+        assert (255, 0, 254) == tuple(rgb[1, 25, 50, :])
+        assert (126, 0, 126) == tuple(rgb[1, 35, 50, :])
+        assert (255, 255, 1) == tuple(rgb[1, 45, 50, :])
+        assert (127, 128, 1) == tuple(rgb[1, 55, 50, :])
+        assert (255, 255, 255) == tuple(rgb[1, 65, 50, :])
+        assert (191, 191, 191) == tuple(rgb[1, 75, 50, :])
+        assert (63, 63, 63) == tuple(rgb[1, 85, 50, :])
+        assert (0, 0, 0) == tuple(rgb[1, 95, 50, :])