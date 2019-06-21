a3a7986a93e203bcf04da5e2f8668366f852b6f8
mrbean-bremen <mrbean-bremen@users.noreply.github.com>
2019-06-21 21:45:41 +0200

    Allow missing padding byte in pixel data (#866)
    * Allow missing padding byte in pixel data
    
    - issue a warning if padding is missing
    - closes #864

--- pydicom/tests/test_numpy_pixel_data.py
+++ pydicom/tests/test_numpy_pixel_data.py
@@ -986,11 +986,21 @@ class TestNumpy_GetPixelData(object):
         # Too short
         ds.PixelData = ds.PixelData[:-1]
         msg = (
-            r"The length of the pixel data in the dataset doesn't match the "
-            r"expected amount \(479999 vs. 480000 bytes\). The dataset may be "
-            r"corrupted or there may be an issue with the pixel data handler."
+            r"The length of the pixel data in the dataset \(479999 bytes\) "
+            r"doesn't match the expected length \(480000 bytes\). "
+            r"The dataset may be corrupted or there may be an issue "
+            r"with the pixel data handler."
         )
         with pytest.raises(ValueError, match=msg):
+            get_pixeldata(ds)
+
+    def test_missing_padding_warns(self):
+        """A warning shall be issued if the padding for odd data is missing."""
+        ds = dcmread(EXPL_8_3_1F_ODD)
+        # remove the padding byte
+        ds.PixelData = ds.PixelData[:-1]
+        msg = "The pixel data length is odd and misses a padding byte."
+        with pytest.warns(UserWarning, match=msg):
             get_pixeldata(ds)
 
     def test_change_photometric_interpretation(self):
