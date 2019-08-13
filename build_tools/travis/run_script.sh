3cf8265ee9202baf251d7b8d5cc7686e06f27bbd
scaramallion <scaramallion@users.noreply.github.com>
2019-08-13 11:55:49 +1000

    Unskip env tests

--- build_tools/travis/run_script.sh
+++ build_tools/travis/run_script.sh
@@ -7,12 +7,14 @@ if [[ "$DISTRIB" == "ubuntu" ]]; then
     bash <(curl --connect-timeout 10 --retry 10 --retry-max-time 0 https://codecov.io/bash) ||
         (sleep 30 && bash <(curl --connect-timeout 10 --retry 10 --retry-max-time 0 https://codecov.io/bash))
 
+    NUMPY="true"
     pip install --force-reinstall numpy
 
     py.test --cov=pydicom -r sx --pyargs pydicom
     bash <(curl --connect-timeout 10 --retry 10 --retry-max-time 0 https://codecov.io/bash) ||
         (sleep 30 && bash <(curl --connect-timeout 10 --retry 10 --retry-max-time 0 https://codecov.io/bash))
 
+    PILLOW="jpeg"
     pip install pillow --global-option="build_ext" --global-option="--disable-jpeg2000"
     python -c "from PIL.features import check_codec; print('JPEG plugin:', check_codec('jpg'))"
     python -c "from PIL.features import check_codec; print('JPEG2k plugin:', check_codec('jpg_2000'))"
@@ -23,6 +25,7 @@ if [[ "$DISTRIB" == "ubuntu" ]]; then
 
     pip uninstall pillow
 
+    PILLOW="both"
     sudo apt-get update
     sudo apt-get install -y libopenjp2-7 libopenjp2-7-dev
     pip install pillow --global-option="build_ext" --global-option="--enable-jpeg2000"
