89922e0097947d32de1b7982bcd847af4d42ea08
scaramallion <scaramallion@users.noreply.github.com>
2019-12-27 18:21:16 +1100

    Fix DicomDir raising exception if there are no records (#1005)

--- pydicom/tests/test_dicomdir.py
+++ pydicom/tests/test_dicomdir.py
@@ -63,10 +63,10 @@ class TestDicomDir(object):
         with pytest.warns(UserWarning, match='Invalid transfer syntax*'):
             dcmread(BIGENDIAN_TEST_FILE)
 
-    def test_missing_patient(self):
-        with pytest.raises(InvalidDicomError,
-                           match=r'Missing PATIENT record\(s\) in DICOMDIR'):
-            dcmread(get_testdata_file('DICOMDIR-nopatient'))
+    def test_empty(self):
+        """Test that an empty DICOMDIR can be read."""
+        ds = dcmread(get_testdata_file('DICOMDIR-empty.dcm'))
+        assert [] == ds.DirectoryRecordSequence
 
     def test_invalid_transfer_syntax_strict_mode(self):
         config.enforce_valid_values = True
