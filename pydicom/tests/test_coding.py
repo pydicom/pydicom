15300fc92c90df94aa073fd6c540705ecee7c6ee
darcymason <darcymason@gmail.com>
2019-12-30 11:19:27 -0500

    Code Style fixes for coding test files

--- pydicom/tests/test_coding.py
+++ pydicom/tests/test_coding.py
@@ -7,18 +7,17 @@ from pydicom.uid import UID
 
 
 class TestCode(unittest.TestCase):
-
     def setUp(self):
         super(TestCode, self).setUp()
-        self._value = '373098007'
-        self._meaning = 'Mean Value of population'
-        self._scheme_designator = 'SCT'
+        self._value = "373098007"
+        self._meaning = "Mean Value of population"
+        self._scheme_designator = "SCT"
 
     def test_construction_kwargs(self):
         c = Code(
             value=self._value,
             scheme_designator=self._scheme_designator,
-            meaning=self._meaning
+            meaning=self._meaning,
         )
         assert c.value == self._value
         assert c.scheme_designator == self._scheme_designator
@@ -26,12 +25,12 @@ class TestCode(unittest.TestCase):
         assert c.scheme_version is None
 
     def test_construction_kwargs_optional(self):
-        version = 'v1.0'
+        version = "v1.0"
         c = Code(
             value=self._value,
             scheme_designator=self._scheme_designator,
             meaning=self._meaning,
-            scheme_version=version
+            scheme_version=version,
         )
         assert c.value == self._value
         assert c.scheme_designator == self._scheme_designator
@@ -46,7 +45,7 @@ class TestCode(unittest.TestCase):
         assert c.scheme_version is None
 
     def test_construction_args_optional(self):
-        version = 'v1.0'
+        version = "v1.0"
         c = Code(self._value, self._scheme_designator, self._meaning, version)
         assert c.value == self._value
         assert c.scheme_designator == self._scheme_designator
@@ -60,17 +59,15 @@ class TestCode(unittest.TestCase):
 
     def test_not_equal(self):
         c1 = Code(self._value, self._scheme_designator, self._meaning)
-        c2 = Code('373099004', 'SCT', 'Median Value of population')
+        c2 = Code("373099004", "SCT", "Median Value of population")
         assert c1 != c2
 
     def test_equal_ignore_meaning(self):
         c1 = Code(self._value, self._scheme_designator, self._meaning)
-        c2 = Code(self._value, self._scheme_designator, 'bla bla bla')
+        c2 = Code(self._value, self._scheme_designator, "bla bla bla")
         assert c1 == c2
 
     def test_equal_equivalent_coding(self):
         c1 = Code(self._value, self._scheme_designator, self._meaning)
-        c2 = Code('R-00317', 'SRT', self._meaning)
+        c2 = Code("R-00317", "SRT", self._meaning)
         assert c1 == c2
-
-
