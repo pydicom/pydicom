e7de71b4a2c7e15b1d23197a00642951cd695d94
scaramallion <scaramallion@users.noreply.github.com>
2020-02-10 23:02:13 +1100

    [MRG] Fix failure to read with empty LUT Descriptor (#1050)

--- pydicom/dataelem.py
+++ pydicom/dataelem.py
@@ -741,9 +741,13 @@ def DataElement_from_raw(raw_data_element, encoding=None):
     except NotImplementedError as e:
         raise NotImplementedError("{0:s} in tag {1!r}".format(str(e), raw.tag))
 
-    if raw.tag in _LUT_DESCRIPTOR_TAGS and value[0] < 0:
+    if raw.tag in _LUT_DESCRIPTOR_TAGS and value:
         # We only fix the first value as the third value is 8 or 16
-        value[0] += 65536
+        try:
+            if value[0] < 0:
+                value[0] += 65536
+        except TypeError:
+            pass
 
     return DataElement(raw.tag, VR, value, raw.value_tell,
                        raw.length == 0xFFFFFFFF, already_converted=True)
