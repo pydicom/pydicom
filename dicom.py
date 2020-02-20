bbbe7801d2e67bd38de78d26403fb6cfcd63e98c
mrbean-bremen <mrbean-bremen@users.noreply.github.com>
2020-02-20 16:10:34 +0100

    Fixed URL to transition guide (#1058)
    - closes #1056

--- dicom.py
+++ dicom.py
@@ -5,7 +5,7 @@ on pydicom 0.9.9 or earlier. E.g. `pip install dicom`.
 Alternatively, most code can easily be converted to pydicom > 1.0 by
 changing import lines from 'import dicom' to 'import pydicom'.
 See the Transition Guide at
-https://pydicom.github.io/pydicom/stable/transition_to_pydicom1.html.
+https://pydicom.github.io/pydicom/stable/old/transition_to_pydicom1.html.
 """
 
 raise ImportError(msg)
