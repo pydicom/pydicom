1675e3634ebefaa1ea5a7bc4188c5b5b91dd015f
darcymason <darcymason@gmail.com>
2020-06-03 17:46:59 -0400

    Add circle, pep8, build_tools back

---
+++ build_tools/circle/build_doc.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+# Decide what kind of documentation build to run, and run it.
+#
+# If the last commit message has a "[doc skip]" marker, do not build
+# the doc. On the contrary if a "[doc build]" marker is found, build the doc
+# instead of relying on the subsequent rules.
+#
+# We always build the documentation for jobs that are not related to a specific
+# PR (e.g. a merge to master or a maintenance branch).
+#
+# If this is a PR, do a full build if there are some files in this PR that are
+# under the "doc/" or "examples/" folders, otherwise perform a quick build.
+#
+# If the inspection of the current commit fails for any reason, the default
+# behavior is to quick build the documentation.
+
+get_build_type() {
+    if [ -z "$CIRCLE_SHA1" ]
+    then
+        echo SKIP: undefined CIRCLE_SHA1
+        return
+    fi
+    commit_msg=$(git log --format=%B -n 1 $CIRCLE_SHA1)
+    if [ -z "$commit_msg" ]
+    then
+        echo QUICK BUILD: failed to inspect commit $CIRCLE_SHA1
+        return
+    fi
+    if [[ "$commit_msg" =~ \[doc\ skip\] ]]
+    then
+        echo SKIP: [doc skip] marker found
+        return
+    fi
+    if [[ "$commit_msg" =~ \[doc\ quick\] ]]
+    then
+        echo QUICK: [doc quick] marker found
+        return
+    fi
+    if [[ "$commit_msg" =~ \[doc\ build\] ]]
+    then
+        echo BUILD: [doc build] marker found
+        return
+    fi
+    if [ -z "$CI_PULL_REQUEST" ]
+    then
+        echo BUILD: not a pull request
+        return
+    fi
+    git_range="origin/master...$CIRCLE_SHA1"
+    git fetch origin master >&2 || (echo QUICK BUILD: failed to get changed filenames for $git_range; return)
+    filenames=$(git diff --name-only $git_range)
+    if [ -z "$filenames" ]
+    then
+        echo QUICK BUILD: no changed filenames for $git_range
+        return
+    fi
+    if echo "$filenames" | grep -q -e ^examples/
+    then
+        echo BUILD: detected examples/ filename modified in $git_range: $(echo "$filenames" | grep -e ^examples/ | head -n1)
+        return
+    fi
+    echo QUICK BUILD: no examples/ filename modified in $git_range:
+    echo "$filenames"
+}
+
+build_type=$(get_build_type)
+if [[ "$build_type" =~ ^SKIP ]]
+then
+    exit 0
+fi
+
+if [[ "$CIRCLE_BRANCH" =~ ^master$|^[0-9]+\.[0-9]+\.X$ && -z "$CI_PULL_REQUEST" ]]
+then
+    # PDF linked into HTML
+    MAKE_TARGET="dist LATEXMKOPTS=-halt-on-error"
+elif [[ "$build_type" =~ ^QUICK ]]
+then
+    MAKE_TARGET=html
+else
+    MAKE_TARGET=html
+fi
+
+# Installing required system packages to support the rendering of math
+# notation in the HTML documentation
+sudo -E apt-get -yq update
+sudo -E apt-get -yq remove texlive-binaries --purge
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes \
+    install dvipng texlive-latex-base texlive-latex-extra \
+    texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended\
+    latexmk
+
+python3 -m venv venv
+. venv/bin/activate
+pip install setuptools numpy matplotlib sphinx pillow sphinx_rtd_theme numpydoc sphinx-gallery sphinx-issues~=1.0
+
+# Build and install pydicom in dev mode
+pip install -e .
+
+# The pipefail is requested to propagate exit code
+set -o pipefail && cd doc && make $MAKE_TARGET 2>&1 | tee ~/log.txt
+
+cd -
+set +o pipefail
---
+++ build_tools/circle/push_doc.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# This script is meant to be called in the "deploy" step defined
+# in .circleci/config.yml. See https://circleci.com/docs/2.0 for more details.
+
+# We have three possibily workflows:
+#   If the git branch is 'master' then we want to commit and merge the dev/
+#       docs on gh-pages
+#   If the git branch is [0-9].[0.9].X (i.e. 0.9.X, 1.0.X, 1.2.X, 41.21.X) then
+#        we want to commit and merge the major.minor/ docs on gh-pages
+#   If the git branch is anything else then we just want to test that committing
+#       the changes works so that any issues can be debugged
+
+function doc_clone_commit {
+    # Clone the pydicom/$DOC_BRANCH branch, update the $DIR directory
+    #   by deleting existing content and copying the most recent version from the
+    #   $CIRCLE_BRANCH, then commit the changes with message $MSG
+    # Note that we use [skip ci] to tell CircleCI not to build the commit
+    MSG="Updating the docs in $DIR/ for branch: $CIRCLE_BRANCH, commit $CIRCLE_SHA1 [skip ci]"
+
+    # CircleCI version 2.0 builds the project in $HOME/project, i.e.:
+    #   /home/circleci/project/pydicom/dataset.py
+    #   note the base directory for the repo is 'project' not 'pydicom'
+
+    # Test to see if $HOME/pydicom exists, if not then clone it from the repo
+    #   with CircleCI v2.0 it will always be the case that we need to clone
+    cd $HOME
+    if [ ! -d $CIRCLE_PROJECT_REPONAME ]
+    then
+        git clone -b $DOC_BRANCH --single-branch $CIRCLE_REPOSITORY_URL
+    fi
+
+    cd $CIRCLE_PROJECT_REPONAME
+    git reset --hard origin/$DOC_BRANCH
+    git rm -rf $DIR/ && rm -rf $DIR/
+    cp -R $HOME/project/doc/_build/html $DIR
+    git config --global user.email $EMAIL
+    git config --global user.name $USERNAME
+    git config --global push.default matching
+    git add -f $DIR/
+    git commit -m "$MSG" $DIR
+}
+
+# Test that the vars have been set
+if [ -z ${CIRCLE_BRANCH+x} ]; then echo "CIRCLE_BRANCH is unset"; fi
+if [ -z ${CIRCLE_SHA1+x} ]; then echo "CIRCLE_SHA1 is unset"; fi
+if [ -z ${CIRCLE_REPOSITORY_URL+x} ]; then echo "CIRCLE_REPOSITORY_URL is unset"; fi
+if [ -z ${CIRCLE_PROJECT_REPONAME+x} ]; then echo "CIRCLE_PROJECT_REPONAME is unset"; fi
+if [ -z ${HOME+x} ]; then echo "HOME is unset"; fi
+if [ -z ${EMAIL+x} ]; then echo "EMAIL is unset"; fi
+if [ -z ${USERNAME+x} ]; then echo "USERNAME is unset"; fi
+
+DOC_BRANCH=gh-pages
+
+# Determine which of the three workflows to take
+if [ "$CIRCLE_BRANCH" = "master" ]
+then
+    # build of current master
+    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
+    # Changes are made to dev/ directory
+    DIR=dev
+    doc_clone_commit
+    git push origin $DOC_BRANCH
+    echo "Push complete"
+elif [[ "$CIRCLE_BRANCH" =~ ^[0-9]+\.[0-9]+\.X$ ]]
+then
+    # build of release, matches branch name against 0.1.X, 91.235.X, etc
+    echo "Performing commit and push to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
+    # Strip off .X from branch name, so changes will go to 0.1/, 91.235/, etc
+    DIR="${CIRCLE_BRANCH::-2}"
+    doc_clone_commit
+    git push origin $DOC_BRANCH
+    echo "Push complete"
+else
+    # build pull release, should be regex ^pull\/[0-9]+$ but lets run against
+    #   everything that doesn't match the other two workflows
+    echo "Testing commit only to $CIRCLE_PROJECT_REPONAME/$DOC_BRANCH for $CIRCLE_BRANCH"
+    # Changes are made to dev/ directory but not merged
+    DIR=dev
+    doc_clone_commit
+    echo "Test complete, changes NOT pushed"
+fi
