c4f6b6f15c3d8b62379ac2ba999a8b03b364ce7e
darcymason <darcymason@gmail.com>
2020-06-08 12:51:23 -0400

    Fix style

--- pydicom/tests/test_charset.py
+++ pydicom/tests/test_charset.py
@@ -107,7 +107,7 @@ class TestCharset:
         ds.read_encoding = None
         ds.SpecificCharacterSet = 'Unsupported'
         with pytest.warns(
-            UserWarning, 
+            UserWarning,
             match=("Unknown encoding 'Unsupported' "
                    "- using default encoding instead")
         ):
@@ -300,8 +300,9 @@ class TestCharset:
             elem = DataElement(0x00100010, 'PN',
                                b'Dionysios=\x1b\x2d\x46'
                                b'\xc4\xe9\xef\xed\xf5\xf3\xe9\xef\xf2')
-            pydicom.charset.decode_element(elem,
-                                   ['ISO 2022 IR 100', 'ISO_2022_IR+126'])
+            pydicom.charset.decode_element(
+                elem, ['ISO 2022 IR 100', 'ISO_2022_IR+126']
+            )
             assert 'Dionysios=Διονυσιος' == elem.value
 
     def test_multi_charset_default_value(self):
@@ -335,9 +336,9 @@ class TestCharset:
                    b'\x1b\x2d\x4C'
                    b'\xbb\xee\xda\x63\x65\xdc\xd1\x79\x70\xd3')
         elem = DataElement(0x00100060, 'PN', encoded)
-        pydicom.charset.decode_element(elem, ['ISO 2022 IR 100',
-                                      'ISO 2022 IR 144',
-                                      'ISO 2022 IR 126'])
+        pydicom.charset.decode_element(
+            elem, ['ISO 2022 IR 100', 'ISO 2022 IR 144', 'ISO 2022 IR 126']
+        )
         assert ['Buc^Jérôme', 'Διονυσιος', 'Люкceмбypг'] == elem.value
 
     def test_single_byte_multi_charset_text(self):
@@ -356,9 +357,9 @@ class TestCharset:
                            b'\xc4\xe9\xef\xed\xf5\xf3\xe9\xef\xf2\\'
                            b'\x1b\x2d\x4C'
                            b'\xbb\xee\xda\x63\x65\xdc\xd1\x79\x70\xd3')
-        pydicom.charset.decode_element(elem, ['ISO 2022 IR 100',
-                                      'ISO 2022 IR 144',
-                                      'ISO 2022 IR 126'])
+        pydicom.charset.decode_element(
+            elem, ['ISO 2022 IR 100', 'ISO 2022 IR 144', 'ISO 2022 IR 126']
+        )
         assert ['Buc^Jérôme', 'Διονυσιος', 'Люкceмбypг'] == elem.value
 
     @pytest.mark.parametrize('encoding, decoded, raw_data', ENCODED_NAMES)
