f463f7d66fd98dadc9f529b4cd3c3a07ccd1f5c1
scaramallion <scaramallion@users.noreply.github.com>
2020-07-23 10:05:26 +1000

    Revert unwanted behaviour in pillow handler

--- pydicom/pixel_data_handlers/pillow_handler.py
+++ pydicom/pixel_data_handlers/pillow_handler.py
@@ -171,7 +171,7 @@ def get_pixeldata(ds: "Dataset") -> "numpy.ndarray":
     if transfer_syntax in PillowJPEG2000TransferSyntaxes:
         # Pillow converts N-bit data to 8- or 16-bit unsigned data,
         # See Pillow src/libImaging/Jpeg2KDecode.c::j2ku_gray_i
-        shift = ds.BitsAllocated - j2k_precision
+        shift = ds.BitsAllocated - ds.BitsStored
         if j2k_precision and j2k_precision != ds.BitsStored:
             warnings.warn(
                 f"The (0028,0101) 'Bits Stored' value ({ds.BitsStored}-bit) "
@@ -181,6 +181,7 @@ def get_pixeldata(ds: "Dataset") -> "numpy.ndarray":
 
         if config.APPLY_J2K_CORRECTIONS and j2k_precision:
             # Corrections based on J2K data
+            shift = ds.BitsAllocated - j2k_precision
             if not j2k_sign and j2k_sign != ds.PixelRepresentation:
                 # Convert unsigned J2K data to 2's complement
                 numpy.right_shift(arr, shift, out=arr)
