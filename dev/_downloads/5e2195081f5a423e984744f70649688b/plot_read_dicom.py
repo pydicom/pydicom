dc0024673bf66e36c8cb529abe4028d6c28df08b
glemaitre <g.lemaitre58@gmail.com>
2020-08-02 22:51:02 +0000

    Updating the docs in dev/ for branch: master, commit 37fc4dd4ee6b0b2dc882ae2e81d1b4597d0d5c4d [skip ci]

--- dev/_downloads/5e2195081f5a423e984744f70649688b/plot_read_dicom.py
+++ dev/_downloads/5e2195081f5a423e984744f70649688b/plot_read_dicom.py
@@ -1,7 +1,7 @@
 """
-=======================================
-Read DICOM and ploting using matplotlib
-=======================================
+==================================
+Read a Dataset and plot Pixel Data
+==================================
 
 This example illustrates how to open a DICOM file, print some dataset
 information, and show it using matplotlib.
@@ -12,38 +12,30 @@ information, and show it using matplotlib.
 # license : MIT
 
 import matplotlib.pyplot as plt
-import pydicom
-from pydicom.data import get_testdata_files
+from pydicom import dcmread
+from pydicom.data import get_testdata_file
 
-print(__doc__)
-
-filename = get_testdata_files('CT_small.dcm')[0]
-dataset = pydicom.dcmread(filename)
+fpath = get_testdata_file('CT_small.dcm')
+ds = dcmread(fpath)
 
 # Normal mode:
 print()
-print("Filename.........:", filename)
-print("Storage type.....:", dataset.SOPClassUID)
+print(f"File path........: {fpath}")
+print(f"SOP Class........: {ds.SOPClassUID} ({ds.SOPClassUID.name})")
 print()
 
-pat_name = dataset.PatientName
+pat_name = ds.PatientName
 display_name = pat_name.family_name + ", " + pat_name.given_name
-print("Patient's name...:", display_name)
-print("Patient id.......:", dataset.PatientID)
-print("Modality.........:", dataset.Modality)
-print("Study Date.......:", dataset.StudyDate)
-
-if 'PixelData' in dataset:
-    rows = int(dataset.Rows)
-    cols = int(dataset.Columns)
-    print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
-        rows=rows, cols=cols, size=len(dataset.PixelData)))
-    if 'PixelSpacing' in dataset:
-        print("Pixel spacing....:", dataset.PixelSpacing)
+print(f"Patient's Name...: {display_name}")
+print(f"Patient ID.......: {ds.PatientID}")
+print(f"Modality.........: {ds.Modality}")
+print(f"Study Date.......: {ds.StudyDate}")
+print(f"Image size.......: {ds.Rows} x {ds.Columns}")
+print(f"Pixel Spacing....: {ds.PixelSpacing}")
 
 # use .get() if not sure the item exists, and want a default value if missing
-print("Slice location...:", dataset.get('SliceLocation', "(missing)"))
+print(f"Slice location...: {ds.get('SliceLocation', '(missing)')}")
 
 # plot the image using matplotlib
-plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
+plt.imshow(ds.pixel_array, cmap=plt.cm.gray)
 plt.show()
