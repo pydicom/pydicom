fe4c99b207981375bdec57d34b8d9f9dbee7a058
scaramallion <scaramallion@users.noreply.github.com>
2020-10-08 07:28:28 +1100

    Refactor and type valuerep classes (#1213)
    * Refactor DA, DT, TM
    * Add type hints for value classes
    * Add workaround for PyPy TM pickling issue

--- pydicom/tests/test_valuerep.py
+++ pydicom/tests/test_valuerep.py
@@ -4,6 +4,12 @@
 
 import copy
 from datetime import datetime, date, time, timedelta, timezone
+from decimal import Decimal
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import sys
 
 from pydicom.tag import Tag
 from pydicom.values import convert_value
@@ -18,25 +24,17 @@ import pytest
 
 from pydicom.valuerep import PersonName
 
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 badvr_name = get_testdata_file("badVR.dcm")
 default_encoding = "iso8859"
 
 
-@pytest.mark.skipif(
-    platform.python_implementation() == "PyPy",
-    reason="PyPy has trouble with this pickle",
-)
 class TestTM:
     """Unit tests for pickling TM"""
-
     def test_pickling(self):
         # Check that a pickled TM is read back properly
         x = pydicom.valuerep.TM("212223")
+        assert time(21, 22, 23) == x
         x.original_string = "hello"
         assert "hello" == x.original_string
         assert time(21, 22, 23) == x
@@ -46,27 +44,131 @@ class TestTM:
         assert x.original_string == x2.original_string
         assert str(x) == str(x2)
 
+    def test_str(self):
+        """Test str(TM)."""
+        x = pydicom.valuerep.TM("212223")
+        assert "212223" == str(x)
+        del x.original_string
+        assert not hasattr(x, 'original_string')
+        assert "21:22:23" == str(x)
+
+    def test_new_empty_str(self):
+        """Test converting an empty string."""
+        x = pydicom.valuerep.TM('')
+        assert x is None
+
+    def test_new_str_conversion(self):
+        """Test converting strings to times."""
+        x = pydicom.valuerep.TM('00')
+        assert "00" == str(x)
+        assert time(0, 0, 0) == x
+        x = pydicom.valuerep.TM('23')
+        assert "23" == str(x)
+        assert time(23, 0, 0) == x
+        msg = r"Unable to convert non-conformant value '24' to 'TM' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.TM('24')
+
+        x = pydicom.valuerep.TM('0000')
+        assert "0000" == str(x)
+        assert time(0, 0, 0) == x
+        x = pydicom.valuerep.TM('2359')
+        assert "2359" == str(x)
+        assert time(23, 59, 0) == x
+        msg = r"Unable to convert non-conformant value '2360' to 'TM' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.TM('2360')
+
+        x = pydicom.valuerep.TM('000000')
+        assert "000000" == str(x)
+        assert time(0, 0, 0) == x
+        # Valid DICOM TM seconds range is 0..60, but time is 0..59
+        msg = (
+            r"'datetime.time' doesn't allow a value of '60' for the "
+            r"seconds component, changing to '59'"
+        )
+        with pytest.warns(UserWarning, match=msg):
+            x = pydicom.valuerep.TM('235960')
+        assert "235960" == str(x)
+        assert time(23, 59, 59) == x
+
+        msg = r"Unable to convert non-conformant value '235' to 'TM' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.TM('235')
+
+    def test_new_obj_conversion(self):
+        """Test other conversion attempts."""
+        assert pydicom.valuerep.TM(None) is None
+        x = pydicom.valuerep.TM("010203.123456")
+        assert time(1, 2, 3, 123456) == pydicom.valuerep.TM(x)
+        assert x == pydicom.valuerep.TM(x)
+        x = pydicom.valuerep.TM(time(1, 2, 3))
+        assert isinstance(x, pydicom.valuerep.TM)
+        assert time(1, 2, 3) == x
+
+        msg = r"Unable to convert '123456' to 'TM' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.TM(123456)
+
 
 class TestDT:
     """Unit tests for pickling DT"""
-
     def test_pickling(self):
         # Check that a pickled DT is read back properly
         x = pydicom.valuerep.DT("19111213212123")
+        assert datetime(1911, 12, 13, 21, 21, 23) == x
         x.original_string = "hello"
         data1_string = pickle.dumps(x)
         x2 = pickle.loads(data1_string)
         assert x == x2
         assert x.original_string == x2.original_string
         assert str(x) == str(x2)
+
+    def test_new_empty_str(self):
+        """Test converting an empty string."""
+        x = pydicom.valuerep.DT('')
+        assert x is None
+
+    def test_new_obj_conversion(self):
+        """Test other conversion attempts."""
+        assert pydicom.valuerep.DT(None) is None
+        x = pydicom.valuerep.DT("10010203")
+        assert datetime(1001, 2, 3) == pydicom.valuerep.DT(x)
+        assert x == pydicom.valuerep.DT(x)
+        x = pydicom.valuerep.DT(datetime(1001, 2, 3))
+        assert isinstance(x, pydicom.valuerep.DT)
+        assert datetime(1001, 2, 3) == x
+
+        msg = r"Unable to convert '123456' to 'DT' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.DT(123456)
+
+    def test_new_str_conversion(self):
+        """Test string conversion."""
+        # Valid DICOM TM seconds range is 0..60, but time is 0..59
+        msg = (
+            r"'datetime.datetime' doesn't allow a value of '60' for the "
+            r"seconds component, changing to '59'"
+        )
+        with pytest.warns(UserWarning, match=msg):
+            x = pydicom.valuerep.DT('20010101235960')
+        assert "20010101235960" == str(x)
+        assert datetime(2001, 1, 1, 23, 59, 59) == x
+
+        msg = (
+            r"Unable to convert non-conformant value 'a2000,00,00' to 'DT' "
+            r"object"
+        )
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.DT("a2000,00,00")
 
 
 class TestDA:
     """Unit tests for pickling DA"""
-
     def test_pickling(self):
         # Check that a pickled DA is read back properly
         x = pydicom.valuerep.DA("19111213")
+        assert date(1911, 12, 13) == x
         x.original_string = "hello"
         data1_string = pickle.dumps(x)
         x2 = pickle.loads(data1_string)
@@ -74,10 +176,23 @@ class TestDA:
         assert x.original_string == x2.original_string
         assert str(x) == str(x2)
 
+    def test_new_obj_conversion(self):
+        """Test other conversion attempts."""
+        assert pydicom.valuerep.DA(None) is None
+        x = pydicom.valuerep.DA("10010203")
+        assert date(1001, 2, 3) == pydicom.valuerep.DA(x)
+        assert x == pydicom.valuerep.DA(x)
+        x = pydicom.valuerep.DA(date(1001, 2, 3))
+        assert isinstance(x, pydicom.valuerep.DA)
+        assert date(1001, 2, 3) == x
+
+        msg = r"Unable to convert '123456' to 'DA' object"
+        with pytest.raises(ValueError, match=msg):
+            pydicom.valuerep.DA(123456)
+
 
 class TestDS:
     """Unit tests for DS values"""
-
     def test_empty_value(self):
         assert DS(None) is None
         assert "" == DS("")
@@ -93,7 +208,6 @@ class TestDS:
 
 class TestDSfloat:
     """Unit tests for pickling DSfloat"""
-
     def test_pickling(self):
         # Check that a pickled DSFloat is read back properly
         x = pydicom.valuerep.DSfloat(9.0)
@@ -119,10 +233,25 @@ class TestDSfloat:
         val = pydicom.valuerep.DSfloat("1.1")
         assert '"1.1"' == repr(val)
 
+    def test_DSfloat(self):
+        """Test creating a value using DSfloat."""
+        x = pydicom.valuerep.DSfloat('1.2345')
+        y = pydicom.valuerep.DSfloat(x)
+        assert x == y
+        assert y == x
+        assert 1.2345 == y
+        assert "1.2345" == y.original_string
+
+    def test_DSdecimal(self):
+        """Test creating a value using DSdecimal."""
+        x = pydicom.valuerep.DSdecimal('1.2345')
+        y = pydicom.valuerep.DSfloat(x)
+        assert 1.2345 == y
+        assert "1.2345" == y.original_string
+
 
 class TestDSdecimal:
     """Unit tests for pickling DSdecimal"""
-
     def test_pickling(self):
         # Check that a pickled DSdecimal is read back properly
         # DSdecimal actually prefers original_string when
@@ -143,13 +272,37 @@ class TestDSdecimal:
         config.allow_DS_float = True
         assert 9 == pydicom.valuerep.DSdecimal(9.0)
 
+    def test_new_empty_str(self):
+        """Test passing an empty string."""
+        assert pydicom.valuerep.DSdecimal('') is None
+
+    def test_DSfloat(self):
+        """Test creating a value using DSfloat."""
+        x = pydicom.valuerep.DSdecimal('1.2345')
+        y = pydicom.valuerep.DSdecimal(x)
+        assert x == y
+        assert y == x
+        assert Decimal("1.2345") == y
+        assert "1.2345" == y.original_string
+
+    def test_DSdecimal(self):
+        """Test creating a value using DSdecimal."""
+        x = pydicom.valuerep.DSfloat('1.2345')
+        y = pydicom.valuerep.DSdecimal(x)
+        assert Decimal(1.2345) == y
+        assert "1.2345" == y.original_string
+
+    def test_repr(self):
+        """Test repr(DSdecimal)."""
+        x = pydicom.valuerep.DSdecimal('1.2345')
+        assert '"1.2345"' == repr(x)
+
 
 class TestIS:
     """Unit tests for IS"""
-
     def test_empty_value(self):
         assert IS(None) is None
-        assert "" == IS("")
+        assert IS("") is None
 
     def test_valid_value(self):
         assert 42 == IS(42)
@@ -180,7 +333,12 @@ class TestIS:
         assert x.real == x2.real
 
     def test_overflow(self, enforce_valid_values):
-        with pytest.raises(OverflowError, match="Value exceeds DICOM limits*"):
+        msg = (
+            r"Elements with a VR of IS must have a value between -2\*\*31 "
+            r"and \(2\*\*31 - 1\). Set 'config.enforce_valid_values' to False "
+            r"to override the value check"
+        )
+        with pytest.raises(OverflowError, match=msg):
             pydicom.valuerep.IS(3103050000)
 
     def test_str(self):
@@ -203,7 +361,6 @@ class TestIS:
 class TestBadValueRead:
     """Unit tests for handling a bad value for a VR
        (a string in a number VR here)"""
-
     def setup(self):
         class TagLike:
             pass
@@ -241,7 +398,6 @@ class TestBadValueRead:
 class TestDecimalString:
     """Unit tests unique to the use of DS class
        derived from python Decimal"""
-
     @pytest.fixture(autouse=True)
     def ds_decimal(self):
         original = config.use_DS_decimal
@@ -259,12 +415,14 @@ class TestDecimalString:
         # invalid
         valid_str = "-9.81338674e-006"
         ds = valuerep.DS(valid_str)
+        assert isinstance(ds, valuerep.DSdecimal)
         assert len(str(ds)) <= 16
 
         # Now the input string is too long but decimal.Decimal can convert it
         # to a valid 16-character string
         long_str = "-0.000000981338674"
         ds = valuerep.DS(long_str)
+        assert isinstance(ds, valuerep.DSdecimal)
         assert len(str(ds)) <= 16
 
     def test_invalid_decimal_strings(self, enforce_valid_values):
@@ -456,7 +614,8 @@ class TestPersonName:
 
         # Test that next() doesn't work without instantiating an iterator
         pn4 = PersonName("SomeName")
-        with pytest.raises(AttributeError):
+        msg = r"'PersonName' object is not an iterator"
+        with pytest.raises(TypeError, match=msg):
             next(pn4)
 
     def test_iterator(self):
@@ -471,6 +630,9 @@ class TestPersonName:
         for i, c in enumerate(pn1):
             assert name_str[i] == c
 
+        for s in iter(PersonName(name_str)):
+            pass
+
     def test_contains(self):
         """Test that characters can be check if they are within the name"""
         pn1 = PersonName("John^Doe")
@@ -481,7 +643,6 @@ class TestPersonName:
 
 class TestDateTime:
     """Unit tests for DA, DT, TM conversion to datetime objects"""
-
     def setup(self):
         config.datetime_conversion = True
 
@@ -563,3 +724,18 @@ class TestDateTime:
         # Assert `tm` equals to no `time`
         tm = valuerep.TM("")
         assert tm is None
+
+
+def test_person_name_unicode_warns():
+    """Test deprecation warning for PersonNameUnicode."""
+    if sys.version_info[:2] < (3, 7):
+        from pydicom.valuerep import PersonNameUnicode
+    else:
+        msg = (
+            r"'PersonNameUnicode' is deprecated and will be removed in "
+            r"pydicom v3.0, use 'PersonName' instead"
+        )
+        with pytest.warns(DeprecationWarning, match=msg):
+            from pydicom.valuerep import PersonNameUnicode
+
+        assert PersonNameUnicode == PersonName
