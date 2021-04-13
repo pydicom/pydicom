0949488c8912b13e6ad6078afa93f2a86cd53538
scaramallion <scaramallion@users.noreply.github.com>
2021-04-14 06:54:56 +1000

    Memory and speed improvements for colourspace conversion (#1351)

--- pydicom/pixel_data_handlers/util.py
+++ pydicom/pixel_data_handlers/util.py
@@ -574,13 +574,17 @@ def apply_windowing(
 
 
 def convert_color_space(
-    arr: "np.ndarray", current: str, desired: str
+    arr: "np.ndarray", current: str, desired: str, per_frame: bool = False
 ) -> "np.ndarray":
     """Convert the image(s) in `arr` from one color space to another.
 
     .. versionchanged:: 1.4
 
         Added support for ``YBR_FULL_422``
+
+    .. versionchanged:: 2.2
+
+        Added `per_frame` keyword parameter.
 
     Parameters
     ----------
@@ -596,6 +600,9 @@ def convert_color_space(
         The desired color space, should be a valid value for (0028,0004)
         *Photometric Interpretation*. One of ``'RGB'``, ``'YBR_FULL'``,
         ``'YBR_FULL_422'``.
+    per_frame : bool, optional
+        If ``True`` and the input array contains multiple frames then process
+        each frame individually to reduce memory usage. Default ``False``.
 
     Returns
     -------
@@ -635,9 +642,14 @@ def convert_color_space(
         converter = _converters[current][desired]
     except KeyError:
         raise NotImplementedError(
-            "Conversion from {0} to {1} is not supported."
-            .format(current, desired)
+            f"Conversion from {current} to {desired} is not supported."
         )
+
+    if len(arr.shape) == 4 and per_frame:
+        for idx, frame in enumerate(arr):
+            arr[idx] = converter(frame)
+
+        return arr
 
     return converter(arr)
 
@@ -670,16 +682,16 @@ def _convert_RGB_to_YBR_FULL(arr: "np.ndarray") -> "np.ndarray":
         [[+0.299, -0.299 / 1.772, +0.701 / 1.402],
          [+0.587, -0.587 / 1.772, -0.587 / 1.402],
          [+0.114, +0.886 / 1.772, -0.114 / 1.402]],
-        dtype=np.float
+        dtype=np.float32
     )
 
-    arr = np.dot(arr, rgb_to_ybr)
+    arr = np.matmul(arr, rgb_to_ybr, dtype=np.float32)
     arr += [0.5, 128.5, 128.5]
     # Round(x) -> floor of (arr + 0.5) : 0.5 added in previous step
-    arr = np.floor(arr)
+    np.floor(arr, out=arr)
     # Max(0, arr) -> 0 if 0 >= arr, arr otherwise
     # Min(arr, 255) -> arr if arr <= 255, 255 otherwise
-    arr = np.clip(arr, 0, 255)
+    np.clip(arr, 0, 255, out=arr)
 
     return arr.astype(orig_dtype)
 
@@ -710,18 +722,19 @@ def _convert_YBR_FULL_to_RGB(arr: "np.ndarray") -> "np.ndarray":
         [[1.000, 1.000, 1.000],
          [0.000, -0.114 * 1.772 / 0.587, 1.772],
          [1.402, -0.299 * 1.402 / 0.587, 0.000]],
-        dtype=float
+        dtype=np.float32
     )
 
-    arr = arr.astype(float)
+    arr = arr.astype(np.float32)
     arr -= [0, 128, 128]
-    arr = np.dot(arr, ybr_to_rgb)
 
     # Round(x) -> floor of (arr + 0.5)
-    arr = np.floor(arr + 0.5)
+    np.matmul(arr, ybr_to_rgb, out=arr)
+    arr += 0.5
+    np.floor(arr, out=arr)
     # Max(0, arr) -> 0 if 0 >= arr, arr otherwise
     # Min(arr, 255) -> arr if arr <= 255, 255 otherwise
-    arr = np.clip(arr, 0, 255)
+    np.clip(arr, 0, 255, out=arr)
 
     return arr.astype(orig_dtype)
 
