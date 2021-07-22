9cb072aec64faf55185d724dda3d0843ae52db9e
hackermd <hackermd@protonmail.com>
2021-07-21 20:34:39 -0400

    Handle optional numpy and pillow dependencies

--- pydicom/color.py
+++ pydicom/color.py
@@ -1,113 +1,149 @@
-import logging
 from io import BytesIO
 
-import numpy as np
-from PIL import Image, ImageCms
-from PIL.ImageCms import (
-    applyTransform,
-    getProfileDescription,
-    getProfileName,
-    ImageCmsProfile,
-    ImageCmsTransform,
-    isIntentSupported,
-)
+from pydicom.config import logger, have_numpy
+
+if have_numpy:
+    import numpy as np
+
+try:
+    from PIL import Image, ImageCms
+    from PIL.ImageCms import (
+        applyTransform,
+        getProfileDescription,
+        getProfileName,
+        ImageCmsProfile,
+        ImageCmsTransform,
+        isIntentSupported,
+    )
+    have_pillow = True
+except ImportError:
+    have_pillow = False
 
 
-logger = logging.getLogger(__name__)
+if have_numpy and have_pillow:
 
+    class ColorManager(object):
 
-class ColorManager(object):
+        """Class for color management using ICC profiles."""
 
-    """Class for color management using ICC profiles."""
+        def __init__(self, icc_profile: bytes):
+            """
 
-    def __init__(self, icc_profile: bytes):
-        """
+            Parameters
+            ----------
+            icc_profile: bytes
+                ICC profile
 
-        Parameters
-        ----------
-        icc_profile: bytes
-            ICC profile
+            Raises
+            ------
+            ValueError
+                When ICC Profile cannot be read.
+            RuntimeError
+                When color management is not possible due to missing
+                dependencies.
 
-        Raises
-        ------
-        ValueError
-            When ICC Profile cannot be read.
+            """
+            try:
+                self._icc_transform = self._build_icc_transform(icc_profile)
+            except OSError:
+                raise ValueError('Could not read ICC Profile.')
 
-        """
-        try:
-            self._icc_transform = self._build_icc_transform(icc_profile)
-        except OSError:
-            raise ValueError('Could not read ICC Profile.')
+        def transform_frame(self, array: np.ndarray) -> np.ndarray:
+            """Transforms a frame by applying the ICC profile.
 
-    def transform_frame(self, array: np.ndarray) -> np.ndarray:
-        """Transforms a frame by applying the ICC profile.
+            Parameters
+            ----------
+            array: numpy.ndarray
+                Pixel data of a color image frame in form of an array with
+                dimensions (Rows x Columns x SamplesPerPixel)
 
-        Parameters
-        ----------
-        array: numpy.ndarray
-            Pixel data of a color image frame in form of an array with
-            dimensions (Rows x Columns x SamplesPerPixel)
+            Returns
+            -------
+            numpy.ndarray
+                Color corrected pixel data of a image frame in form of an array
+                with dimensions (Rows x Columns x SamplesPerPixel)
 
-        Returns
-        -------
-        numpy.ndarray
-            Color corrected pixel data of a image frame in form of an array
-            with dimensions (Rows x Columns x SamplesPerPixel)
+            Raises
+            ------
+            ValueError
+                When `array` does not have 3 dimensions and thus does not represent
+                a color image frame.
 
-        Raises
-        ------
-        ValueError
-            When `array` does not have 3 dimensions and thus does not represent
-            a color image frame.
+            """
+            if array.ndim != 3:
+                raise ValueError(
+                    'Array has incorrect dimensions for a color image frame.'
+                )
+            image = Image.fromarray(array)
+            applyTransform(image, self._icc_transform, inPlace=True)
+            return np.asarray(image)
 
-        """
-        if array.ndim != 3:
-            raise ValueError(
-                'Array has incorrect dimensions for a color image frame.'
+        @staticmethod
+        def _build_icc_transform(icc_profile: bytes) -> ImageCmsTransform:
+            """Builds an ICC Transformation object.
+
+            Parameters
+            ----------
+            icc_profile: bytes
+                ICC Profile
+
+            Returns
+            -------
+            PIL.ImageCms.ImageCmsTransform
+                ICC Transformation object
+
+            """
+            profile: bytes
+            try:
+                profile = ImageCmsProfile(BytesIO(icc_profile))
+            except OSError:
+                raise ValueError('Cannot read ICC Profile in image metadata.')
+            name = getProfileName(profile).strip()
+            description = getProfileDescription(profile).strip()
+            logger.debug(f'found ICC Profile "{name}": "{description}"')
+
+            logger.debug('build ICC Transform')
+            intent = ImageCms.INTENT_RELATIVE_COLORIMETRIC
+            if not isIntentSupported(
+                profile,
+                intent=intent,
+                direction=ImageCms.DIRECTION_INPUT
+            ):
+                raise ValueError(
+                    'ICC Profile does not support desired '
+                    'color transformation intent.'
+                )
+            return ImageCms.buildTransform(
+                inputProfile=profile,
+                outputProfile=ImageCms.createProfile('sRGB'),
+                inMode='RGB',  # according to PS3.3 C.11.15.1.1
+                outMode='RGB'
             )
-        image = Image.fromarray(array)
-        applyTransform(image, self._icc_transform, inPlace=True)
-        return np.asarray(image)
 
-    @staticmethod
-    def _build_icc_transform(icc_profile: bytes) -> ImageCmsTransform:
-        """Builds an ICC Transformation object.
+else:
 
-        Parameters
-        ----------
-        icc_profile: bytes
-            ICC Profile
+    class ColorManager(object):
 
-        Returns
-        -------
-        PIL.ImageCms.ImageCmsTransform
-            ICC Transformation object
+        """Class for color management using ICC profiles."""
 
-        """
-        profile: bytes
-        try:
-            profile = ImageCmsProfile(BytesIO(icc_profile))
-        except OSError:
-            raise ValueError('Cannot read ICC Profile in image metadata.')
-        name = getProfileName(profile).strip()
-        description = getProfileDescription(profile).strip()
-        logger.debug(f'found ICC Profile "{name}": "{description}"')
+        def __init__(self, icc_profile: bytes):
+            """
 
-        logger.debug('build ICC Transform')
-        intent = ImageCms.INTENT_RELATIVE_COLORIMETRIC
-        if not isIntentSupported(
-            profile,
-            intent=intent,
-            direction=ImageCms.DIRECTION_INPUT
-        ):
-            raise ValueError(
-                'ICC Profile does not support desired '
-                'color transformation intent.'
+            Parameters
+            ----------
+            icc_profile: bytes
+                ICC profile
+
+            Raises
+            ------
+            ValueError
+                When ICC Profile cannot be read.
+            RuntimeError
+                When color management is not possible due to missing
+                dependencies.
+
+            """
+            raise RuntimeError(
+                'Color management requires "numpy" and "Pillow" packages '
+                'to be installed.'
             )
-        return ImageCms.buildTransform(
-            inputProfile=profile,
-            outputProfile=ImageCms.createProfile('sRGB'),
-            inMode='RGB',  # according to PS3.3 C.11.15.1.1
-            outMode='RGB'
-        )
-
--- pydicom/tests/test_color.py
+++ pydicom/tests/test_color.py
@@ -1,31 +1,63 @@
-import numpy as np
 import pytest
-from PIL.ImageCms import ImageCmsProfile, createProfile
 
 from pydicom.color import ColorManager
+from pydicom.config import have_numpy
+
+if have_numpy:
+    import numpy as np
+
+try:
+    from PIL.ImageCms import ImageCmsProfile, createProfile
+    have_pillow = True
+except ImportError:
+    have_pillow = False
 
 
 class TestColorManager:
 
-    def setup(self):
-        self._icc_profile = ImageCmsProfile(createProfile('sRGB')).tobytes()
-
+    @pytest.mark.skipif(
+        not(have_numpy and have_pillow),
+        reason="numpy and pillow are not installed"
+    )
     def test_construction(self):
-        ColorManager(self._icc_profile)
+        icc_profile = ImageCmsProfile(createProfile('sRGB')).tobytes()
+        ColorManager(icc_profile)
 
+    @pytest.mark.skipif(
+        have_numpy and have_pillow,
+        reason="numpy and pillow are installed"
+    )
+    def test_construction_missing_dependencies(self):
+        with pytest.raises(RuntimeError):
+            ColorManager(b'')
+
+    @pytest.mark.skipif(
+        not(have_numpy and have_pillow),
+        reason="numpy and pillow are not installed"
+    )
     def test_construction_without_profile(self):
         with pytest.raises(TypeError):
             ColorManager()  # type: ignore
 
+    @pytest.mark.skipif(
+        not(have_numpy and have_pillow),
+        reason="numpy and pillow are not installed"
+    )
     def test_transform_frame(self):
-        manager = ColorManager(self._icc_profile)
+        icc_profile = ImageCmsProfile(createProfile('sRGB')).tobytes()
+        manager = ColorManager(icc_profile)
         frame = np.ones((10, 10, 3), dtype=np.uint8) * 255
         output = manager.transform_frame(frame)
         assert output.shape == frame.shape
         assert output.dtype == frame.dtype
 
+    @pytest.mark.skipif(
+        not(have_numpy and have_pillow),
+        reason="numpy and pillow are not installed"
+    )
     def test_transform_frame_wrong_shape(self):
-        manager = ColorManager(self._icc_profile)
+        icc_profile = ImageCmsProfile(createProfile('sRGB')).tobytes()
+        manager = ColorManager(icc_profile)
         frame = np.ones((10, 10), dtype=np.uint8) * 255
         with pytest.raises(ValueError):
             manager.transform_frame(frame)
