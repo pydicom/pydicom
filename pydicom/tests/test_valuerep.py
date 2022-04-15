3d987378a9984d60a9f1df947830bb770ec2d6fa
bernardopericacho <bernardopericacho@gmail.com>
2022-04-15 15:57:42 -0700

    Improve example using family_comma_given method from PersonName VR

--- pydicom/tests/test_valuerep.py
+++ pydicom/tests/test_valuerep.py
@@ -1090,7 +1090,8 @@ class TestPersonName:
             "\033$)C\373\363^\033$)C\321\316\324\327="
             "\033$)C\310\253^\033$)C\261\346\265\277"
         )
-        assert ("Hong", "Gildong", "Andrews") == (pn.family_name, pn.given_name, pn.middle_name)
+        assert ("Hong", "Gildong", "Andrews") == \
+               (pn.family_name, pn.given_name, pn.middle_name)
         assert "Hong^Gildong^Andrews" == pn.alphabetic
         assert "\033$)C\373\363^\033$)C\321\316\324\327" == pn.ideographic
         assert "\033$)C\310\253^\033$)C\261\346\265\277" == pn.phonetic
