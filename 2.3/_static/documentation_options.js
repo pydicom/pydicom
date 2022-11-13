1a8c717e25e49d4560550ec41d08571948ce5eff
darcymason <darcymason@gmail.com>
2022-11-13 15:13:20 +0000

    Updating the docs in 2.3/ for branch: 2.3.X, commit ce7424b427fed66be1cc39f54beb70152715e215 [skip ci]

--- 2.3/_static/documentation_options.js
+++ 2.3/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.0',
-    LANGUAGE: 'None',
+    VERSION: '2.3.1',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
