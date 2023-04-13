951629deca145acc43235eb185c97c219044f7db
Ryan Thompson <ryan@gradienthealth.io>
2023-04-13 13:14:38 -0400

    update version name

--- pydicom/_version.py
+++ pydicom/_version.py
@@ -3,7 +3,7 @@ import re
 from typing import cast, Match
 
 
-__version__: str = "2.3.0:GradientHealth"
+__version__: str = "2.3.0-dev"
 
 result = cast(Match[str], re.match(r"(\d+\.\d+\.\d+).*", __version__))
 __version_info__ = tuple(result.group(1).split("."))
