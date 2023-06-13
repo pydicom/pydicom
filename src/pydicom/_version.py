e0ea30d78c6131010f9e029c7213562dead17e86
Darcy Mason <darcymason@gmail.com>
2023-06-13 15:09:26 -0400

    Use pydicom version from metadata

--- src/pydicom/_version.py
+++ src/pydicom/_version.py
@@ -1,9 +1,9 @@
 """Pure python package for DICOM medical file reading and writing."""
 import re
 from typing import cast, Match
+from importlib.metadata import version
 
-
-__version__: str = '3.0.0.dev0'
+__version__: str = version("pydicom")
 
 result = cast(Match[str], re.match(r'(\d+\.\d+\.\d+).*', __version__))
 __version_info__ = tuple(result.group(1).split('.'))
