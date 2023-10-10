b6b4e21865051e2f6c55939d3edc91522a1c6dff
mrbean-bremen <hansemrbean@googlemail.com>
2023-10-10 20:58:31 +0200

    Fix example for adding private dictionary entries
    - add section about adding dictionary entries to user guide

--- examples/metadata_processing/plot_add_dict_entries.py
+++ examples/metadata_processing/plot_add_dict_entries.py
@@ -1,46 +1,36 @@
 """
-=====================================================
-Add dictionary items in the standard DICOM dictionary
-=====================================================
+=============================================================
+Add private dictionary items to the standard DICOM dictionary
+=============================================================
 
-This examples illustrates how to add dictionary items to the 'standard' DICOM
-dictionary dynamically.
-
-.. warning::
-
-   Note that this is not a recommended solution in general but it is useful as
-   a demonstration or for in-house programs only, or to get around elements
-   missing from pydicom's DICOM dictionaries.
-
+This examples illustrates how to add private dictionary items to the DICOM
+dictionary dynamically. This allows to add unknown private tags to a new dataset,
+or reading private tags from an existing dataset not present in the pydicom
+private dictionary.
 """
 
-# authors : Darcy Mason
-#           Guillaume Lemaitre <g.lemaitre58@gmail.com>
+# authors : Darcy Mason and pydicom contributors
 # license : MIT
 
 
-from pydicom.datadict import DicomDictionary, keyword_dict
+from pydicom.datadict import add_private_dict_entries
 from pydicom.dataset import Dataset
 
 print(__doc__)
 
-# Define items as (VR, VM, description, is_retired flag, keyword)
-#   Leave is_retired flag blank.
+# Define items as (VR, VM, description, is_retired flag)
+# Leave is_retired flag blank.
 new_dict_items = {
-    0x10011001: ("UL", "1", "Test One", "", "TestOne"),
-    0x10011002: ("OB", "1", "Test Two", "", "TestTwo"),
-    0x10011003: ("UI", "1", "Test Three", "", "TestThree"),
+    0x10011001: ("UL", "1", "Test One", ""),
+    0x10011002: ("OB", "1", "Test Two", ""),
+    0x10011003: ("UI", "1", "Test Three", ""),
 }
 
-# Update the dictionary itself
-DicomDictionary.update(new_dict_items)
-
-# Update the reverse mapping from name to tag
-new_names_dict = dict([(val[4], tag) for tag, val in new_dict_items.items()])
-keyword_dict.update(new_names_dict)
+# add the entries to the private dictionary, using the correct private creator string
+add_private_dict_entries(private_creator="ACME 3.1", new_entries_dict=new_dict_items)
 
 # Test that it is working
-ds = Dataset()  # or could get one from dcmread, etc
+ds = Dataset()
 
 ds.TestOne = 42
 ds.TestTwo = "12345"
