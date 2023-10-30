6279275559a606c49f8edc1e78d221edd68fa5d7
Gino Notto <gnotto@butterflynetinc.com>
2023-10-30 13:26:59 -0400

    formatting

--- tests/test_buffer_utils.py
+++ tests/test_buffer_utils.py
@@ -5,21 +5,19 @@ from pydicom.util.buffers import buffer_length, read_bytes, reset_buffer_positio
 
 
 class TestBufferUtils:
-
-    @pytest.mark.parametrize("data", (
-            b'\x00\x01\x02\x03',
-            b'',
+    @pytest.mark.parametrize(
+        "data",
+        (
+            b"\x00\x01\x02\x03",
+            b"",
             # big value
-            bytes(("test" * 10_000), encoding="utf-8")
-    ))
-    @pytest.mark.parametrize("chunk_size", (
-        None,
-        1,
-        100_000_000
-    ))
+            bytes(("test" * 10_000), encoding="utf-8"),
+        ),
+    )
+    @pytest.mark.parametrize("chunk_size", (None, 1, 100_000_000))
     def test_reading_data_from_a_buffer(self, data, chunk_size):
         buffer = BytesIO(data)
-        data = b''
+        data = b""
 
         kwargs = {}
         if chunk_size:
@@ -29,48 +27,45 @@ class TestBufferUtils:
         for chunk in read_bytes(buffer, **kwargs):
             data += chunk
 
-
         assert data == data
 
-
     def test_reading_data_from_a_buffer_from_middle_of_buffer(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         buffer.seek(2)
 
-        data = b''
+        data = b""
         for chunk in read_bytes(buffer):
             data += chunk
 
-        assert data == b'\x02\x03'
+        assert data == b"\x02\x03"
 
-    
-    @pytest.mark.parametrize("chunk_size", (
-        0,
-        -1,
-    ))
+    @pytest.mark.parametrize(
+        "chunk_size",
+        (
+            0,
+            -1,
+        ),
+    )
     def test_invalid_inputs_to_read_bytes(self, chunk_size):
         with pytest.raises(AssertionError, match="chunk_size must be > 0"):
             next(read_bytes(BytesIO(), chunk_size=chunk_size))
 
-    
     def test_reading_bytes_from_buffer_does_not_reset_buffer_position(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         for _ in read_bytes(buffer):
             pass
 
         assert buffer.tell() == 4
 
-
     def test_reading_length_of_buffer(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         assert buffer_length(buffer) == 4
 
         # reset back to position
         assert buffer.tell() == 0
 
-
     def test_reading_length_of_buffer_from_last_position(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         buffer.seek(2)
 
         assert buffer_length(buffer) == 2
@@ -78,26 +73,23 @@ class TestBufferUtils:
         # reset back to position
         assert buffer.tell() == 2
 
-    
     def test_reset_buffer_position(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         with reset_buffer_position(buffer):
             buffer.read()
 
         assert buffer.tell() == 0
 
-
     def test_reset_buffer_position_from_last_position(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         buffer.seek(2)
         with reset_buffer_position(buffer):
             buffer.read()
 
         assert buffer.tell() == 2
 
-
     def test_reset_buffer_position_yields_starting_position(self):
-        buffer = BytesIO(b'\x00\x01\x02\x03')
+        buffer = BytesIO(b"\x00\x01\x02\x03")
         buffer.seek(2)
         with reset_buffer_position(buffer) as start_position:
             assert start_position == 2
