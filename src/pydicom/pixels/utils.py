490e5afc51d9c20383c42cf540e53eba40896cdb
Chris Bridge <chrisbridge44@googlemail.com>
2025-03-22 12:45:23 -0400

    Enable single bit compression

--- src/pydicom/pixels/utils.py
+++ src/pydicom/pixels/utils.py
@@ -681,6 +681,9 @@ def decompress(
     elem.is_undefined_length = False
     elem.VR = VR.OB if ds.BitsAllocated <= 8 else VR.OW
 
+    if ds.BitsAllocated == 1:
+        elem.value = pack_bits(elem.value)
+
     # Update the transfer syntax
     ds.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
 
@@ -1283,8 +1286,8 @@ def iter_pixels(
             f.seek(file_offset)
 
 
-def pack_bits(arr: "np.ndarray", pad: bool = True) -> bytes:
-    """Pack a binary :class:`numpy.ndarray` for use with *Pixel Data*.
+def pack_bits(arr: "np.ndarray | bytes | bytearray", pad: bool = True) -> bytes:
+    """Pack a binary :class:`numpy.ndarray` or bytes for use with *Pixel Data*.
 
     Should be used in conjunction with (0028,0100) *Bits Allocated* = 1.
 
@@ -1293,17 +1296,27 @@ def pack_bits(arr: "np.ndarray", pad: bool = True) -> bytes:
         Added the `pad` keyword parameter and changed to allow `arr` to be
         2 or 3D.
 
+    .. versionchanged:: 3.1
+
+        Allow packing of bytes and bytearray objects when numpy is not
+        installed.
+
     Parameters
     ----------
-    arr : numpy.ndarray
-        The :class:`numpy.ndarray` containing 1-bit data as ints. `arr` must
-        only contain integer values of 0 and 1 and must have an 'uint'  or
-        'int' :class:`numpy.dtype`. For the sake of efficiency it's recommended
+    arr : numpy.ndarray | bytes
+        The :class:`numpy.ndarray`, `bytes`, or `bytesarray` containing 1-bit
+        data as ints/bytes. If it is a :class:`numpy.ndarray`, `arr` must only
+        contain integer values of 0 and 1 and must have an 'uint'  or 'int'
+        :class:`numpy.dtype`. If it is a `bytes` or `bytesarray`, each byte
+        represents a pixel and must have a value of either 0 or 1 (i.e.
+        `b'\x00'` or `b'\x01'`). For the sake of efficiency it's recommended
         that the length of `arr` be a multiple of 8 (i.e. that any empty
-        bit-padding to round out the byte has already been added). The input
-        `arr` should either be shaped as (rows, columns) or (frames, rows,
-        columns) or the equivalent 1D array used to ensure that the packed
-        data is in the correct order.
+        bit-padding to round out the byte has already been added). For arrays,
+        the input `arr` should either be shaped as (rows, columns) or (frames,
+        rows, columns) or the equivalent 1D array used to ensure that the
+        packed data is in the correct order. For `bytes` or `bytesarray`,
+        pixels should be ordered in the same order as a 1D array, i.e. column
+        index changes most frequently, then row index, then frame index.
     pad : bool, optional
         If ``True`` (default) then add a null byte to the end of the packed
         data to ensure even length, otherwise no padding will be added.
@@ -1324,6 +1337,38 @@ def pack_bits(arr: "np.ndarray", pad: bool = True) -> bytes:
     :dcm:`Section 8.1.1<part05/chapter_8.html#sect_8.1.1>` and
     :dcm:`Annex D<part05/chapter_D.html>`
     """
+    if isinstance(arr, bytes | bytearray):
+        if len(arr) == 0:
+            return b""
+
+        if HAVE_NP:
+            # Form an array because the numpy implementation is more efficient
+            arr = np.frombuffer(arr, dtype=np.uint8)
+        else:
+            # Fallback for when numpy is not installed (slower)
+            out_array = bytearray()
+            for offset in range(0, len(arr), 8):
+                packed_int = 0
+                for i in range(8):
+                    byte_index = offset + i
+                    if byte_index >= len(arr):
+                        break
+                    b = arr[byte_index]
+                    if b not in (0, 1):
+                        raise ValueError(
+                            "Only binary arrays (containing ones or zeroes) "
+                            "can be packed."
+                        )
+                    if b == 1:
+                        packed_int |= 1 << i
+
+                out_array.append(packed_int)
+
+            if pad and len(out_array) % 2 == 1:
+                out_array.append(0)
+
+            return bytes(out_array)
+
     if arr.shape == (0,):
         return b""
 
