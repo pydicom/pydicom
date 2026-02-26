efb791bf3ce9a25b3ce48fbedb55f30b43323a84
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-02-26 06:43:33 +0100

    Apply ruff/flake8-comprehensions rule C401
    Unnecessary generator (rewrite as a set comprehension)

--- src/pydicom/sr/codedict.py
+++ src/pydicom/sr/codedict.py
@@ -38,9 +38,7 @@ def _filtered(source: Iterable[str], filters: Iterable[str]) -> list[str]:
 
     filters = [f.lower() for f in filters]
 
-    return sorted(
-        set(val for val in source if any((f in val.lower()) for f in filters))
-    )
+    return sorted({val for val in source if any((f in val.lower()) for f in filters)})
 
 
 CIDValueType = dict[str, tuple[str, list[int]]]
